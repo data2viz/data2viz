8b2513bc164f67e1b40b29e5b14e00fc9850d6c1
gzoritchak <g.zoritchak@gmail.com>
2017-03-23 22:33:53 +0100

    Trigonometry constants added.

---
+++ core/src/trigonometry/trigonometry.kt
@@ -0,0 +1,13 @@
+package trigonometry
+
+
+import kotlin.js.Math
+
+val EPS = 1e-6
+val EPS2 = EPS * EPS
+val PI = Math.PI
+val halfPI = PI / 2
+val THETA = PI * 2
+val THETA_EPS = THETA - EPS
+val radians = PI / 180
+val degrees = 180 / PI
