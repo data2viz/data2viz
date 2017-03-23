a9c0edb77c2396702e1f9a62548c1c77f4977db1
gzoritchak <g.zoritchak@gmail.com>
2017-03-23 17:21:30 +0100

    creation of module core.

---
+++ core/src/namespace.kt
@@ -0,0 +1,9 @@
+class namespace {
+    companion object {
+        val svg = "http://www.w3.org/2000/svg"
+        //        val     xhtml: d3_nsXhtml,
+        val xlink = "http://www.w3.org/1999/xlink"
+        val xml = "http://www.w3.org/XML/1998/namespace"
+        val xmlns = "http://www.w3.org/2000/xmlns/"
+    }
+}
