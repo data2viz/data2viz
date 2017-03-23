e32442bc0415e7990342779ecf12298b56e87d1b
gzoritchak <g.zoritchak@gmail.com>
2017-03-23 22:33:00 +0100

    cubicIn, cubicOut, cubicInOut.

--- interpolate/src/number.kt
+++ interpolate/src/number.kt
@@ -3,3 +3,4 @@
 fun interpolateNumber(a:Number, b:Number) = {t:Double -> a.toDouble() + t * (b.toDouble() - a.toDouble()) }
 
 //fun uninterpolaleNumber(a:Number, b: Number) =
+fun identity(a:Double) = a
