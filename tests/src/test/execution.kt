015777f19a6a1597a9839d2b70d7814289d508bf
gzoritchak <g.zoritchak@gmail.com>
2017-03-23 17:18:51 +0100

    Improvement: result layout

--- tests/src/test/execution.kt
+++ tests/src/test/execution.kt
@@ -3,31 +3,50 @@ package test
 import kotlin.browser.document
 import kotlin.browser.window
 
-
-fun htmlExecution(vararg testBase: TestBase){
+fun htmlExecution(vararg testBase: TestBase) {
     val body = window.document.querySelector("body")!!
     testBase.forEach {
         body.append(document.createElement("h2").apply { textContent = it::class.simpleName })
-        it.tests
-                .map { it.execute() }
-                .forEach { body.appendChild(it.toHtml()) }
+        it.tests.forEach {
+
+
+            val resultDescription = document.createElement("span").apply {
+                className = "resultDescription"
+            }
+
+            val divTest = document.createElement("div").apply {
+
+
+                appendChild(resultDescription)
+                appendChild(document.createElement("span").apply {
+                    className = "testName"
+                    textContent = it.name
+                })
+
+            }
+            body.appendChild(divTest)
+
+            val result = it.execute()
+
+            val okOrKo: String = when (result) {
+                is TestResult.KO -> "KO"
+                else -> " OK"
+            }
+
+            divTest.className = "testResult $okOrKo"
+
+            resultDescription.textContent = okOrKo
+
+
+            if (result is TestResult.KO) {
+                divTest.appendChild(
+                        document.createElement("div").apply {
+                            className = "error"
+                            textContent = result.message
+                        }
+                )
+            }
+        }
+
     }
 }
-
-fun TestResult.toHtml() =
-        document.createElement("div").apply {
-            className = "testResult ${this@toHtml.result()}"
-
-            appendChild(document.createElement("span").apply {
-                className = "testName"
-                textContent = name
-            })
-
-            appendChild(document.createElement("span").apply {
-                className = "resultDescription"
-                textContent = when (this@toHtml) {
-                    is TestResult.KO -> this@toHtml.message
-                    else -> " OK"
-                }
-            })
-        }
