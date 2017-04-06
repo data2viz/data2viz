d48c03da9e8ae48621bb289c7397c8d40197ab7a
gzoritchak <g.zoritchak@gmail.com>
2017-04-06 02:20:36 +0200

    Binding perf test: Filtering __data__ property on 2500 elements in 5 ms.

---
+++ all/src/bindingPerfs.kt
@@ -0,0 +1,39 @@
+import io.data2viz.selection.SelectionTests
+import io.data2viz.selection.append
+import org.w3c.dom.asList
+import test.DomUtils
+import test.htmlExecution
+import kotlin.browser.document
+import kotlin.js.Date
+
+
+fun bindingPerfs() {
+
+    data class TimeAndResult<T>(val time:Double, val result: T)
+
+    fun <T> time(block:() -> T):TimeAndResult<T> {
+        val time = Date().getTime()
+        val ret = block()
+        return TimeAndResult(Date().getTime() - time, ret)
+    }
+
+    val count = 2500
+    val creationTime = time {
+        (1..count).forEach {
+            DomUtils.body.append("div") {
+                asDynamic().__data__ = it
+            }
+        }
+
+    }
+    console.log("$count éléments créés en ${creationTime.time} ms")
+
+    val selectionTime = time {
+        DomUtils.body.querySelectorAll("div").asList().filter {
+            (it.asDynamic().__data__ as Int) % 2 == 0
+        }.size
+    }
+
+    console.log("Filtrage de ${selectionTime.result} en ${selectionTime.time} ms")
+
+}
