884d66e4a54620a2ed21b5ec57eec6dcfadd2e45
gzoritchak <g.zoritchak@gmail.com>
2017-04-24 00:44:04 +0200

    Adding uninterpolate to numbers.kt

--- interpolate/src/io/data2viz/interpolate/NumberTests.kt
+++ interpolate/src/io/data2viz/interpolate/NumberTests.kt
@@ -1,54 +1,21 @@
 package io.data2viz.interpolate
 
-import io.data2viz.interpolate.identity
-import io.data2viz.interpolate.*
 import test.StringSpec
-import kotlin.browser.document
 
-class EaseTests : StringSpec() {
+class NumberTests : StringSpec() {
     init {
         "interpolate" {
-            val x = interpolateNumber(10, 20)
-            x(0.2) shouldBe 12.0
+            val f = interpolateNumber(10, 20)
+            f(0.2) shouldBe 12.0
         }
 
-        "io.data2viz.interpolate.identity"  { testAndGraph() }
-        "quad"      { testAndGraph(::quad) }
-        "cubicIn"   { testAndGraph(::cubicIn) }
-        "cubicOut"  { testAndGraph(::cubicOut) }
-        "cubicInOut"{ testAndGraph(::cubicInOut) }
-        "sin"       { testAndGraph(::sin) }
-        "circleIn"  { testAndGraph(::circleIn) }
-        "circleOut" { testAndGraph(::circleOut) }
+        "uninterpolate" {
+            val f = uninterpolate(10, 20)
+            f(12) shouldBe 0.2
+        }
+
+
+
     }
 
-    fun testAndGraph(function: (Double) -> Double = ::identity) {
-
-        function(0.0) shouldBe (.0 plusOrMinus 0.01)
-        function(1.0) shouldBe (1.0 plusOrMinus 0.01)
-
-        fun node(name: String) = document.createElementNS(namespace.svg, name)
-
-
-        val body = document.querySelector("body")!!
-        body.appendChild(
-                node("svg").apply {
-                    setAttribute("width", "100")
-                    setAttribute("height", "100")
-
-                    appendChild(
-                            node("path").apply {
-                                setAttribute("stroke", "black")
-                                setAttribute("fill", "transparent")
-                                val path = (0..100).map {
-                                    val x = it
-                                    val y = 100 - function(it.toDouble() / 100) * 100
-                                    "L $x $y"
-                                }.joinToString(separator = " ")
-                                setAttribute("d", "M 0 100 " + path)
-                            }
-                    )
-                }
-        )
-    }
 }
--- interpolate/src/io/data2viz/interpolate/number.kt
+++ interpolate/src/io/data2viz/interpolate/number.kt
@@ -1,6 +1,5 @@
 package io.data2viz.interpolate
 
 fun interpolateNumber(a:Number, b:Number) = {t:Double -> a.toDouble() + t * (b.toDouble() - a.toDouble()) }
-
-//fun uninterpolaleNumber(a:Number, b: Number) =
+fun uninterpolate(start:Number, end: Number) :(Number) -> Number = {x -> (x.toDouble() - start.toDouble())/(end.toDouble() - start.toDouble())}
 fun identity(a:Double) = a
