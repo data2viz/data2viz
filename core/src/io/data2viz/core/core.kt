fef3d5ec88cc72cbe4a5a97875ea113f7ae46bc5
gzoritchak <g.zoritchak@gmail.com>
2017-04-25 13:01:48 +0200

    Refactoring Point: Double -> Number

--- core/src/io/data2viz/core/core.kt
+++ core/src/io/data2viz/core/core.kt
@@ -3,12 +3,12 @@ package io.data2viz.core
 
 
 
-data class Point(val x: Double = 0.0, val y: Double = 0.0) {
+data class Point(val x: Number = 0.0, val y: Number = 0.0) {
 
     companion object {
         val origin = Point()
     }
-    operator fun plus(speed: Speed) = Point(x + speed.vx, y + speed.vy)
+    operator fun plus(speed: Speed) = Point(x.toDouble() + speed.vx, y.toDouble() + speed.vy)
 }
 
 data class Speed(val vx: Double = 0.0, val vy: Double = 0.0) {
