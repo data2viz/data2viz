dae7bf11698875308d76ec033f7cb1bcbe3aecf0
pmariac <pmariac@gmail.com>
2017-05-12 16:32:32 +0200

    premiers tests du "relax" sur les poylgones de voronoi

--- experiments/src/io/data2viz/experiments/fantasymap/fantasymap.kt
+++ experiments/src/io/data2viz/experiments/fantasymap/fantasymap.kt
@@ -1,16 +1,19 @@
 package io.data2viz.experiments.fantasymap
 
-import io.data2viz.color.colors.steelblue
-import io.data2viz.experiments.animate.Point
+import io.data2viz.color.colors.white
+import io.data2viz.core.Point
 import io.data2viz.svg.svg
+import kotlin.browser.window
 import kotlin.js.Math
 
 external fun launchVoronoi(): Array<Array<Array<Double>>>
+external fun setSites(sites: Array<Array<Number>>):Unit
 
-external fun setSites(sites: Array<Array<Double>>):Unit
+fun Point.toArray():Array<Number> = arrayOf(this.x, this.y)
+fun Array<Double>.toPoint() = Point(this[0], this[1])
 
 data class Params(
-        val npts:Int = 20,//   16384,
+        val npts:Int = 200,//   16384,
         val ncities:Int = 15,
         val nterrs:Int = 5,
         val fontSizeRegion:Int = 40,
@@ -28,62 +31,52 @@ data class Extent(
         val height:Int = 1
 )
 
-fun generatePoints(n:Int, extent:Extent):MutableList<Point> {
-    val pts = mutableListOf<Point>()
-    for (i in 0..n) {
-        pts.add(Point(Math.random() * extent.width, Math.random() * extent.height))
+fun listsToArray(list:List<Point>): Array<Array<Number>> {
+    return list.map {it.toArray()}.toTypedArray()
+}
+
+fun generatePoints(params:Params): Array<Array<Number>> {
+    val pts:Array<Array<Number>> = emptyArray()
+    for (i in 0 .. params.npts - 1) {
+        val pt:Point = Point(Math.random()* params.mapWidth, Math.random() * params.mapHeight)
+        pts[i] = pt.toArray()
     }
     return pts
 }
 
-/*fun generateGoodPoints(n:Int, extent:Extent): List<Point> {
-    var pts = generatePoints(n, extent);
-    pts.sortBy { it.x }
-    return improvePoints(pts, 1, extent);
-}*/
-
-/*fun voronoi(pts: List<Point>, passNumber: Int, extent: Extent) {
-    var w = extent.width/2;
-    var h = extent.height/2;
-    return d3.voronoi().extent([[-w, -h], [w, h]])(pts);
-}*/
-
-/**
- * Build Voronoi graphs from points, then
- */
-fun improvePoints(pts: Array<Array<Double>>): List<Point> {
-    var res:List<Point> = mutableListOf()
-    /*for (i in 0 .. passNumber) {
-        //res = voronoi(pts).map { centroid(it) }
-    }*/
-    return res
+fun improvePoints(polygons: Array<Array<Array<Double>>>): List<Point> {
+    return polygons.map { centroid(it) }
 }
-
-
 
 fun centroid(pts: Array<Array<Double>>): Point {
     var x = 0.0
     var y = 0.0
-    pts.forEach { x += it[0]; y += it[1] }
+    pts.forEachIndexed { index, pt ->
+        x += pt[0]; y += pt[1]
+    }
     return Point(x / pts.size, y / pts.size)
 }
 
-fun doMap(params: Params, polygons: Array<Array<Array<Double>>>) {
+fun doMap(params: Params) {
+
+    val polygons = launchVoronoi()
 
     val svg = svg {
+
         width = params.mapWidth
         height = params.mapHeight
 
+        rect {
+            transform {
+                translate(Point(0,0))
+            }
+            width = params.mapWidth
+            height = params.mapHeight
+            fill = white
+        }
+
         polygons.forEach { polygon ->
             polygon.forEachIndexed { index, vertex ->
-                circle {
-                    transform {
-                        translate()
-                    }
-                    r = 2
-                    fill = steelblue
-                }
-
                 if (index < polygon.size - 1) {
                     val nextVertex = polygon[index+1]
                     line(vertex[0], vertex[1], nextVertex[0], nextVertex[1])
@@ -91,6 +84,8 @@ fun doMap(params: Params, polygons: Array<Array<Array<Double>>>) {
             }
         }
     }
+
+    setSites(listsToArray(improvePoints(polygons)))
 }
 
 
@@ -98,25 +93,9 @@ fun buildFantasyMap(){
     println("building fantasy map")
 
     val params = Params()
-    //doMap(params)
 
-    var pts:Array<Array<Double>> = emptyArray()
+    setSites(generatePoints(params))
+    doMap(params)
 
-    for (i in 0 .. params.npts - 1) {
-        val pt:Point = Point(Math.random()* params.mapWidth, Math.random() * params.mapHeight)
-        val ptToJS: Array<Double> = arrayOf()
-        ptToJS[0] = pt.x;
-        ptToJS[1] = pt.y
-        pts[i] = ptToJS
-    }
-
-    setSites(pts)
-
-    println("launch voronoi")
-
-    var polygons = launchVoronoi()
-
-    doMap(params, polygons)
-
-    //val newSites = improvePoints(polygons)
+    window.setInterval({doMap(params)}, 10)
 }
