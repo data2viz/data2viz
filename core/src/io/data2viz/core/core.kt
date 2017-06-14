7014059a4767a7722fb729fdb672f844ade2e01d
pmariac <pmariac@gmail.com>
2017-06-14 20:23:35 +0200

    add plus and div operators for point

--- core/src/io/data2viz/core/core.kt
+++ core/src/io/data2viz/core/core.kt
@@ -9,6 +9,8 @@ data class Point(val x: Number = 0.0, val y: Number = 0.0) {
         val origin = Point()
     }
     operator fun plus(speed: Speed) = Point(x.toDouble() + speed.vx, y.toDouble() + speed.vy)
+    operator fun plus(other: Point) = Point(x.toDouble() + other.x.toDouble(), y.toDouble() + other.y.toDouble())
+    operator fun div(value:Number) = Point(x.toDouble()/value.toDouble(), y.toDouble()/value.toDouble())
 }
 
 data class Speed(val vx: Double = 0.0, val vy: Double = 0.0) {
