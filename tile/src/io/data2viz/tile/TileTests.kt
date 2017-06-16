4ff86f6b705c780f1852bb47ee56abcd2b8b129b
gzoritchak <g.zoritchak@gmail.com>
2017-06-16 13:18:42 +0200

    Transformation of tile API as a DSL.

--- tile/src/io/data2viz/tile/TileTests.kt
+++ tile/src/io/data2viz/tile/TileTests.kt
@@ -2,51 +2,63 @@ package io.data2viz.tile
 
 import io.data2viz.core.Point
 import io.data2viz.core.namespace
+import io.data2viz.math.Angle
+import io.data2viz.math.PI
+import io.data2viz.math.THETA
+import io.data2viz.math.deg
 import io.data2viz.test.DOMExecutionContext
 import io.data2viz.test.ExecutionContext
 import io.data2viz.test.StringSpec
 import kotlin.browser.document
+import kotlin.js.Math
 
 class TileTests : StringSpec() {
 
-    val width = 800.0
-    val height = 200.0
+    data class GeoPoint(val latitude:Angle, val longitude: Angle)
+
+    val GENEVA = GeoPoint( latitude = 46.2.deg, longitude = 6.1667.deg)
+
+
+    /**
+     * http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+     */
+    fun GeoPoint.long2tile(zoom: Int) = ((longitude + PI) / THETA) * Math.pow(2.0,zoom.toDouble())
+    fun GeoPoint.lat2tile(zoom: Int) = (1-Math.log(Math.tan(latitude.deg*Math.PI/180) + 1/Math.cos(latitude.deg*Math.PI/180))/Math.PI)/2 *Math.pow(2.0,zoom.toDouble())
 
     init {
 
         "Should display Geneva"  {
             context ->
             //see http://bl.ocks.org/mbostock/94b9fd26e12c586f342d
-            val tile = tile
-            tile.width = width
-            tile.height = height
-            tile.translation = Point(-3000.0, 29100.0)
-            tile.scale = 200000.0
-
-            val tiles = tile.tiles()
-
-            drawmap(context, tiles)
+            val layout = tilesLayout {
+                width = 800.0
+                height = 200.0
+                tilesCount = 200_000.0
+            }
+            layout.translation = Point (-3000.0, 29100.0)
+//                    (layout.tilesCount / 2) - GENEVA.long2tile(layout.zoom) * layout.tileSize,
+//                    (layout.tilesCount / 2) - GENEVA.lat2tile(layout.zoom) * layout.tileSize)
+            drawmap(context, layout)
         }
     }
 
-    private fun drawmap(context: ExecutionContext, tiles:Tiles) {
+    private fun drawmap(context: ExecutionContext, layout: TilesLayout) {
 
         if (context !is DOMExecutionContext) return
 
         context.element.appendChild(
                 document.createElementNS(namespace.svg, "svg").apply {
-                    setAttribute("width", "$width")
-                    setAttribute("height", "$height")
-                    tiles.tiles.forEach { tile ->
+                    setAttribute("width", "${layout.width}")
+                    setAttribute("height", "${layout.height}")
+                    layout.tiles().forEach { tile ->
                         appendChild(
                                 document.createElementNS(namespace.svg, "image").apply {
-                                    setAttribute("href", "http://a.tile.openstreetmap.org/${tile.z}/${tile.x}/${tile.y}.png")
-                                    setAttribute("x", "${(tile.x.toDouble() + tiles.translation.x.toDouble())*tiles.scale}")
-                                    setAttribute("y", "${(tile.y.toDouble() + tiles.translation.y.toDouble())*tiles.scale}")
-                                    setAttribute("width", "${tiles.scale}")
-                                    setAttribute("height", "${tiles.scale}")
+                                    setAttribute("href", "http://a.tile.openstreetmap.org/${tile.zoom}/${tile.tileX}/${tile.tileY}.png")
+                                    setAttribute("x", "${tile.x}")
+                                    setAttribute("y", "${tile.y}")
+                                    setAttribute("width", "${layout.tileSize}")
+                                    setAttribute("height", "${layout.tileSize}")
                                 }
-
                         )
                     }
                 }
--- tile/src/io/data2viz/tile/tile.kt
+++ tile/src/io/data2viz/tile/tile.kt
@@ -3,53 +3,38 @@ package io.data2viz.tile
 import io.data2viz.core.Point
 import kotlin.js.Math
 
-data class Tile(val x: Number, val y: Number, val z: Number, val tx: Number, val ty: Number)
-data class Tiles(val tiles: MutableList<Tile>, val scale: Double, val translation: Point)
-
 val LN2 = Math.log(2.0)
 val INFINITY: Int = 2147483647
 
-private var origin = Point(0, 0)
-private var end = Point(960, 500)
-private var _translation = (origin + end) / 2
-private var _scale = 256.0
-private var _zoomDelta = 0.0
-private var _wrap = true
+fun tilesLayout(init: TilesLayout.() -> Unit): TilesLayout = TilesLayout().apply(init)
 
-fun computeTiles(): Tiles {
-    val z = Math.max(Math.log(_scale) / LN2 - 8.0, 0.0)
-    val z0 = Math.round(z + _zoomDelta)
-    val j = 1 shl z0
-    val k = Math.pow(2.0, z - z0 + 8.0)
-    val x = _translation.x.toDouble() - (_scale / 2)
-    val y = _translation.y.toDouble() - (_scale / 2)
-    val tilesList: MutableList<Tile> = arrayListOf<Tile>()
+/**
+ * A layout for determining which 256x256 quadtree tiles to display in a rectangular viewport
+ */
+class TilesLayout {
 
-    val minCols = Math.max(if (_wrap) -INFINITY else 0, Math.floor((origin.x.toDouble() - x) / k))
-    val maxCols = Math.min(Math.ceil((end.x.toDouble() - x) / k), if (_wrap) INFINITY else j)
-    val minRows = Math.max(0, Math.floor((origin.y.toDouble() - y) / k))
-    val maxRows = Math.min(Math.ceil((end.y.toDouble() - y) / k), j)
 
-    (minRows..maxRows).forEach { y ->
-        (minCols..maxCols).forEach { x ->
-            tilesList.add(Tile(x = (x % j + j) % j, y = y, z = z0, tx = x * 256, ty = y * 256))
-        }
+    private var stale = true
+
+    private var origin = Point(0, 0)
+    private var end = Point(960, 500)
+
+    var translation = (origin + end) / 2
+
+    var zoomDelta = 0.0
+    set(value) {
+        field = value
+        stale = true
     }
 
-    return Tiles(tilesList, k, Point(x / k, y / k))
-}
-
-
-object tile {
-
-    fun tiles() = computeTiles()
+    private var wrap = true
 
     var width
         get() = end.x.toDouble() - origin.x.toDouble()
         set(value) {
             origin = Point(0, origin.y)
             end = Point(value, end.y)
-            //computeTiles()
+            stale = true
         }
 
     var height
@@ -57,40 +42,112 @@ object tile {
         set(value) {
             origin = Point(origin.x, 0)
             end = Point(end.x, value)
-            //computeTiles()
+            stale = true
         }
 
-    var scale
-        get() = _scale
-        set(value) {
-            _scale = value
-            //computeTiles()
+    /**
+     * The number of tiles used to represent the planet.
+     * See http://wiki.openstreetmap.org/wiki/Zoom_levels
+     * Determines zoom level and tileSize.
+     */
+    var tilesCount: Double = 256.0
+
+    private var _tileSize: Double = 256.0
+    val tileSize:Double
+        get() {
+            eventualyRecompute()
+            return _tileSize
         }
 
-    var zoomDelta
-        get() = _zoomDelta
-        set(value) {
-            _zoomDelta = value
-            //computeTiles()
+    var zoom = 1
+        get() {
+            eventualyRecompute()
+            return field
         }
 
-    var wrap
-        get() = _wrap
-        set(value) {
-            _wrap = value
-            //computeTiles()
-        }
+    var translate:Point? = null
 
-    var translation
-        get() = _translation
-        set(value) {
-            _translation = value
-            //computeTiles()
-        }
 
-    fun setExtent(from: Point, to: Point) {
-        origin = from
-        end = to
-        //computeTiles()
+    private fun eventualyRecompute(){
+
+        if (!stale) return
+        stale = false
+
+        // scale = 200_000 => z = 9,61
+        val z = Math.max(Math.log(tilesCount) / LN2 - 8.0, 0.0)
+
+        //zoom. http://wiki.openstreetmap.org/wiki/Zoom_levels
+        //zoom = 0 => 1 tiles, zoom = 1 => 4 tiles, ...
+        // scale = 200_000 => z = 10
+        zoom = Math.round(z + zoomDelta)
+
+        // k = 195,3125 (tile size)
+        _tileSize = Math.pow(2.0, z - zoom + 8.0)
     }
+
+    /**
+     * Returns the list of tiles defined by the current layout
+     */
+    fun tiles(): List<Tile> {
+
+        if(stale) eventualyRecompute()
+
+        //number of horizontal tile. zoom = 10 => 1024 horizontal tiles
+        val j = 1 shl zoom
+
+        val x = translation.x.toDouble() - (tilesCount / 2)
+        val y = translation.y.toDouble() - (tilesCount / 2)
+        translate = Point(x, y) / _tileSize
+
+        val minCols = Math.max(if (wrap) -INFINITY else 0, Math.floor((origin.x.toDouble() - x) / _tileSize))
+        val maxCols = Math.min(Math.ceil((end.x.toDouble() - x) / _tileSize), if (wrap) INFINITY else j)
+        val minRows = Math.max(0, Math.floor((origin.y.toDouble() - y) / _tileSize))
+        val maxRows = Math.min(Math.ceil((end.y.toDouble() - y) / _tileSize), j)
+
+        val tiles = mutableListOf<Tile>()
+        (minRows..maxRows).forEach { row ->
+            (minCols..maxCols).forEach { col ->
+                tiles.add(
+                        Tile(
+                                layout = this,
+                                tileX = (col % j + j) % j,
+                                tileY = row))
+            }
+        }
+
+        return tiles
+    }
+}
+
+data class Tile(
+
+        /**
+         * The current layout context of this title.
+         */
+        val layout: TilesLayout,
+
+        /**
+         * The integer X coordinate of the tile address. Periodic if wrap is set to true.
+         */
+        val tileX: Int,
+
+        /**
+         * The integer Y coordinate of the tile address.
+         */
+        val tileY: Int) {
+
+
+    /**
+     * The integer Z coordinate of the tile address (zoom level).
+     */
+    val zoom: Int
+        get() = layout.zoom
+
+    val x:Double
+        get() = layout.tileSize * (layout.translate!!.x.toDouble() + tileX)
+
+
+    val y:Double
+        get() = layout.tileSize * (layout.translate!!.y.toDouble() + tileY)
+
 }
