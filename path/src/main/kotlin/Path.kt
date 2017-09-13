be209569292350e8daa3047d62b90d2843743e2f
gzoritchak <g.zoritchak@gmail.com>
2017-09-13 11:36:18 +0200

    Add arcTo and rect to path.

--- path/src/main/kotlin/Path.kt
+++ path/src/main/kotlin/Path.kt
@@ -57,6 +57,69 @@ class Path() {
         cmd += "C$x1,$y1,$x2,$y2,$x,$y"
     }
 
+    fun arcTo(fromX:Number, fromY:Number, toX:Number, toY:Number, radius:Number){
+        val r = radius.toDouble()
+        if (r < 0.0) throw IllegalArgumentException("Negative radius:" + radius)
+
+        val x1 = fromX.toDouble()
+        val y1 = fromY.toDouble()
+        val x2 = toX.toDouble()
+        val y2 = toY.toDouble()
+
+        val x0 = this.x1?.toDouble() ?: .0
+        val y0 = this.y1?.toDouble() ?: .0
+
+        val x21 = x2 - x1
+        val y21 = y2 - y1
+        val x01 = x0 - x1
+        val y01 = y0 - y1
+        val l01_2 = x01 * x01 + y01 * y01
+
+        with(this.x1){
+            //path is empty, introduce private function?
+            if(this == null){
+                // Is this path empty? Move to (x1,y1).
+                this@Path.x1 = x1
+                this@Path.y1 = y1
+                cmd += "M$x1,$y1"
+            }
+            // Or, is (x1,y1) coincident with (x0,y0)? Do nothing.
+            else if (l01_2 <= epsilon){}
+
+            // Or, are (x0,y0), (x1,y1) and (x2,y2) collinear?
+            // Equivalently, is (x1,y1) coincident with (x2,y2)?
+            // Or, is the radius zero? Line to (x1,y1).
+            else if (Math.abs(y01 * x21 - y21 * x01) <= epsilon || r == .0) {
+                this@Path.x1 = x1
+                this@Path.y1 = y1
+                cmd += "L$x1,$y1"
+            }
+
+            // Otherwise, draw an arc!
+            else {
+                val x20 = x2 - x0
+                val y20 = y2 - y0
+                val l21_2 = x21 * x21 + y21 * y21
+                val l20_2 = x20 * x20 + y20 * y20
+                val l21 = Math.sqrt(l21_2)
+                val l01 = Math.sqrt(l01_2)
+                val l = r * Math.tan((pi - Math.acos((l21_2 + l01_2 - l20_2) / (2 * l21 * l01))) / 2)
+                val t01 = l / l01
+                val t21 = l / l21;
+
+                // If the start tangent is not coincident with (x0,y0), line to.
+                if (Math.abs(t01 - 1) > epsilon) {
+                    cmd += "L${x1 + t01 * x01},${y1 + t01 * y01}";
+                }
+
+                this@Path.x1 = x1 + t21 * x21
+                this@Path.y1 = y1 + t21 * y21
+                val yes = if (y01 * x20 > x01 * y20) 1 else 0
+                cmd += "A$r,$r,0,0,$yes,${this@Path.x1},${this@Path.y1}"
+            }
+        }
+    }
+
 
     /**
      *
@@ -76,9 +139,7 @@ class Path() {
         val x0 = x + dx
         val y0 = y + dy
         val cw = if (counterClockWise) 0 else 1
-//        var cw = pow(1.toDouble(),(counterClockWise.toDouble()))
         var da = if(counterClockWise) a0 - a1 else a1 - a0
-
 
         with(x1){
 
@@ -109,5 +170,14 @@ class Path() {
             cmd += "A$r,$r,0,${if (da >= pi) 1 else 0},$cw,$x1,$y1";
         }
     }
+
+    fun rect(x:Number, y:Number, w:Number, h:Number) {
+        x0 = x
+        x1 = x
+        y0 = y
+        y1 = y
+        cmd += "M$x,${y}h${w}v${h}h${-w.toDouble()}Z"
+    }
+
 
 }
--- path/src/test/kotlin/PathTests.kt
+++ path/src/test/kotlin/PathTests.kt
@@ -30,13 +30,14 @@ class PathTests : TestBase() {
 
     @Test
     fun pathMoveToAppendMCommand() {
-        val p = path()
-        p.moveTo(150, 50)
-        p.cmd shouldBe "M150,50"
-        p.lineTo(200, 100)
-        p.cmd shouldBe "M150,50L200,100"
-        p.moveTo(100, 50)
-        p.cmd shouldBe "M150,50L200,100M100,50"
+        with(path()) {
+            moveTo(150, 50)
+            cmd shouldBe "M150,50"
+            lineTo(200, 100)
+            cmd shouldBe "M150,50L200,100"
+            moveTo(100, 50)
+            cmd shouldBe "M150,50L200,100M100,50"
+        }
     }
 
     @Test
@@ -368,6 +369,102 @@ class PathTests : TestBase() {
             moveTo(100, 50)
             arc(100, 100, 50, 3 * Math.PI / 2, 0, true);
             cmd.round() shouldBe "M100,50A50,50,0,1,0,150,100"
+        }
+    }
+
+    @Test
+    fun path_arcTo_with_negative_radius_throws_error() {
+        shouldThrow<IllegalArgumentException> { path().arcTo(1, 1, 1, 1, -1) }
+    }
+
+
+    @Test
+    fun path_arcTo_appends_an_M_command_if_the_path_was_empty() {
+        with(path()) {
+            arcTo(270, 39, 163, 100, 53);
+            cmd.round() shouldBe "M270,39"
+
+        }
+    }
+
+    @Test
+    fun path_arcTo_does_nothing_if_the_previous_point_was_x1_y1() {
+        with(path()) {
+            moveTo(270, 39); arcTo(270, 39, 163, 100, 53);
+            cmd.round() shouldBe "M270,39"
+
+        }
+    }
+
+    @Test
+    fun path_arcTo_appends_an_L_command_if_the_previous_point_x1_y1_and_x2_y2_are_collinear() {
+        with(path()) {
+            moveTo(100, 50); arcTo(101, 51, 102, 52, 10);
+            cmd.round() shouldBe "M100,50L101,51"
+
+        }
+    }
+
+    @Test
+    fun path_arcTo_appends_an_L_command_if_x1_y1_and_x2_y2_are_coincident() {
+        with(path()) {
+            moveTo(100, 50); arcTo(101, 51, 101, 51, 10);
+            cmd.round() shouldBe "M100,50L101,51"
+
+        }
+    }
+
+    @Test
+    fun path_arcTo_appends_an_L_command_if_the_radius_is_zero() {
+        with(path()) {
+            moveTo(270, 182)
+            arcTo(270, 39, 163, 100, 0)
+            cmd.round() shouldBe "M270,182L270,39"
+
+        }
+    }
+
+    @Test
+    fun path_arcTo_appends_L_and_A_commands_if_the_arc_does_not_start_at_the_current_point() {
+        with(path()) {
+            moveTo(270, 182)
+            arcTo(270, 39, 163, 100, 53)
+            cmd.round() shouldBe "M270,182L270,130.222686A53,53,0,0,0,190.750991,84.179342"
+        }
+        with(path()) {
+            moveTo(270, 182)
+            arcTo(270, 39, 363, 100, 53)
+            cmd.round() shouldBe "M270,182L270,137.147168A53,53,0,0,1,352.068382,92.829799"
+
+        }
+    }
+
+    @Test
+    fun path_arcTo_appends_only_an_A_command_if_the_arc_starts_at_the_current_point() {
+        with(path()) {
+            moveTo(100, 100)
+            arcTo(200, 100, 200, 200, 100);
+            cmd.round() shouldBe "M100,100A100,100,0,0,1,200,200"
+
+        }
+    }
+
+    @Test
+    fun path_arcTo_sets_the_last_point_to_be_the_end_tangent_of_the_arc() {
+        with(path()) {
+            moveTo(100, 100)
+            arcTo(200, 100, 200, 200, 50); arc(150, 150, 50, 0, Math.PI);
+            cmd.round() shouldBe "M100,100L150,100A50,50,0,0,1,200,150A50,50,0,1,1,100,150"
+
+        }
+    }
+
+    @Test
+    fun path_rect_appends_M_h_v_h_and_Z_commands() {
+        with(path()){
+            moveTo(150,100)
+            rect(100,200,50,25)
+            cmd shouldBe "M150,100M100,200h50v25h-50Z"
         }
     }
 
