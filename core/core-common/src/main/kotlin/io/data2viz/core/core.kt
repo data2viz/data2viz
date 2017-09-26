f7158e27689e4bc4d9f66c1a5370948c3e6e9c71
gzoritchak <g.zoritchak@gmail.com>
2017-09-26 22:49:25 +0200

    Spliting core code between common and specific js and jvm versions.

---
+++ core/core-common/src/main/kotlin/io/data2viz/core/core.kt
@@ -0,0 +1,18 @@
+package io.data2viz.core
+
+
+data class Point(val x: Double = 0.0, val y: Double = 0.0) {
+
+    companion object {
+        val origin = Point()
+    }
+    operator fun plus(speed: Speed) = Point(x + speed.vx, y + speed.vy)
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+    operator fun div(value:Number) = Point(x/value.toDouble(), y/value.toDouble())
+}
+
+data class Speed(val vx: Double = 0.0, val vy: Double = 0.0) {
+    operator fun plus(speed: Speed) = Speed(vx + speed.vx, vy + speed.vy)
+}
+
+header fun random():Double
---
+++ core/core-common/src/main/kotlin/io/data2viz/core/namespace.kt
@@ -0,0 +1,11 @@
+package io.data2viz.core
+
+class namespace {
+    companion object {
+        val svg = "http://www.w3.org/2000/svg"
+        //        val     xhtml: d3_nsXhtml,
+        val xlink = "http://www.w3.org/1999/xlink"
+        val xml = "http://www.w3.org/XML/1998/io.data2viz.core.namespace"
+        val xmlns = "http://www.w3.org/2000/xmlns/"
+    }
+}
