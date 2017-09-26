7bf3f11cb6dc2000218ea891a5ced7d69df90d24
gzoritchak <g.zoritchak@gmail.com>
2017-09-26 15:44:36 +0200

    Removing binding perf.

--- experiments/src/test/kotlin/entryPoints.kt
+++ experiments/src/test/kotlin/entryPoints.kt
@@ -1,7 +1,6 @@
 import io.data2viz.experiments.fantasymap.buildFinalFantasyMap
 
 
-fun bindingPerfs() = io.data2viz.experiments.bindingPerfs()
 fun fantasyMap()    = buildFinalFantasyMap()
 //fun fantasyVoroinoMap()    = buildVoronoiFantasyMap()
 fun jsPerfs() = io.data2viz.experiments.perfs.jsPerfs()
