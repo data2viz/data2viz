67c23ee8c40c9294401bbad6278bf0502498c1ab
gzoritchak <g.zoritchak@gmail.com>
2017-10-05 12:27:57 +0200

    Addind contour project.

---
+++ contour/d2v-contour-common/src/main/kotlin/io/data2viz/contour/Contour.kt
@@ -0,0 +1,5 @@
+package io.data2viz.contour
+
+
+
+fun thatSTRue() = true
---
+++ contour/d2v-contour-jvm/src/test/kotlin/io/data2viz/contour/ContourTests.kt
@@ -0,0 +1,13 @@
+package io.data2viz.contour
+
+import io.data2viz.test.matchers.Matchers
+import org.junit.Test
+
+
+class ContourTests: Matchers {
+
+
+    @Test fun `should be true`(){
+        thatSTRue() shouldBe true
+    }
+}
