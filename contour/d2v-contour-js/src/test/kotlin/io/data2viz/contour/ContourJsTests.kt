cab54a45c119f7204d9d7f38d779d02f3886a11e
gzoritchak <g.zoritchak@gmail.com>
2017-10-09 22:38:50 +0200

    Adding jsTest and benchmarks.

---
+++ contour/d2v-contour-js/src/test/kotlin/io/data2viz/contour/ContourJsTests.kt
@@ -0,0 +1,36 @@
+package io.data2viz.contour
+
+import io.data2viz.test.TestBase
+import kotlin.test.Test
+
+
+
+class ContourJsTests : TestBase() {
+
+    fun p(x: Number, y: Number) = arrayOf(x.toDouble(), y.toDouble())
+
+
+    @Test
+    fun collinear() {
+        io.data2viz.contour.collinear(p(0, 0), p(1, 1), p(2, 2)) shouldBe true
+        io.data2viz.contour.collinear(p(0, 0), p(1, 1), p(2, 1)) shouldBe false
+        io.data2viz.contour.collinear(p(0, 0), p(2, 2), p(1, 1)) shouldBe true
+    }
+
+}
+
+@JsName("squaredContour")
+fun squaredContour(size: Int) = contour {
+    size(size, size)
+    thresholds = { arrayOf(0.5) }
+}
+
+@JsName("contour")
+fun Contour.jsContour(values: Array<Double>) {
+    contours(values)
+}
+
+
+
+
+
