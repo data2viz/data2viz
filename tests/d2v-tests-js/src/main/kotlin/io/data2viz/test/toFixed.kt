edb1a0c4477bfcbf06d7587ef7435024db4e9468
pierre mariac <pmariac@gmail.com>
2017-11-13 10:32:30 +0100

    change round() method in order to pass build

--- tests/d2v-tests-js/src/main/kotlin/io/data2viz/test/toFixed.kt
+++ tests/d2v-tests-js/src/main/kotlin/io/data2viz/test/toFixed.kt
@@ -5,6 +5,6 @@ import kotlin.math.round
 
 actual fun Double.toFixed(): String {
     return if (abs(this - round(this)) < 1e-6)
-        round(this).toString() + ".0"
+        round(this).toString()
     else this.asDynamic().toFixed(6)
 }
