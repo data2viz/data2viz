dd0bb11b65db14d85ba93d3675cb56cd32abfd39
gzoritchak <g.zoritchak@gmail.com>
2017-11-17 16:32:23 +0100

    Make circleIn more solid.

--- interpolate/d2v-interpolate-common/src/main/kotlin/io/data2viz/interpolate/ease.kt
+++ interpolate/d2v-interpolate-common/src/main/kotlin/io/data2viz/interpolate/ease.kt
@@ -16,5 +16,5 @@ fun sin(x:Double) = 1 - cos(x * halfPI.rad)
 fun poly(e:Double) = {t:Double -> t.pow(e)}
 
 
-fun circleIn(t:Double) = 1 - sqrt(1 - t * t)
+fun circleIn(t:Double) = 1 - sqrt(1 - (t * t).coerceAtMost(1.0))
 fun circleOut(t:Double) = sqrt(1 - (t-1)*(t-1));
