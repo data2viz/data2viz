57479bc6bf80bb2f88745583e75812217a24d21e
gzoritchak <g.zoritchak@gmail.com>
2017-11-22 17:05:02 +0100

    ColorJvm add conversion to/from JfxColor.

--- color/d2v-color-jvm/src/main/kotlin/io/data2viz/color/ColorJVM.kt
+++ color/d2v-color-jvm/src/main/kotlin/io/data2viz/color/ColorJVM.kt
@@ -1,3 +1,9 @@
 package io.data2viz.color
 
 internal actual fun Int.toString(radix: Int): String = Integer.toString(this, radix)
+
+val javafx.scene.paint.Color.d2vColor: Color
+    get() = Color().apply { rgba(red, green, blue, opacity) }
+
+val Color.jfxColor
+    get() =  javafx.scene.paint.Color.rgb(r, g, b, alpha.toDouble())
