2572d6ce0aab3576edd4f49c5dfafc4775641c6c
gzoritchak <g.zoritchak@gmail.com>
2017-11-24 00:25:48 +0100

    Adding explicit return type to Color.jfxColor

--- color/d2v-color-jvm/src/main/kotlin/io/data2viz/color/ColorJVM.kt
+++ color/d2v-color-jvm/src/main/kotlin/io/data2viz/color/ColorJVM.kt
@@ -5,5 +5,5 @@ internal actual fun Int.toString(radix: Int): String = Integer.toString(this, ra
 val javafx.scene.paint.Color.d2vColor: Color
     get() = Color().apply { rgba(red, green, blue, opacity) }
 
-val Color.jfxColor
+val Color.jfxColor: javafx.scene.paint.Color
     get() =  javafx.scene.paint.Color.rgb(r, g, b, alpha.toDouble())
