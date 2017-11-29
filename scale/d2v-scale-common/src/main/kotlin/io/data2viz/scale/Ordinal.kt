b9b3b76ae29e1e217c1a57b7401b434807da6263
pierre mariac <pmariac@gmail.com>
2017-11-29 15:14:41 +0100

    DV-74 : ordinal scale implementation + tests

---
+++ scale/d2v-scale-common/src/main/kotlin/io/data2viz/scale/Ordinal.kt
@@ -0,0 +1,84 @@
+package io.data2viz.scale
+
+/**
+ * Unlike continuous scales, ordinal scales have a discrete domain and range.
+ * For example, an ordinal scale might map a set of named categories to a set of colors, or determine the
+ * horizontal positions of columns in a column chart.
+ */
+open class OrdinalScale<D, R> : RangeableScale<D, R>, TickableScale<D, R> {
+
+    private val index: MutableMap<D, Int> = HashMap()
+    private val innerDomain: MutableList<D> = ArrayList()
+
+    /**
+     * Set : Sets the output value of the scale for unknown input values and returns this scale.
+     * Get : Returns the current unknown value, which defaults to null.
+     *
+     * The behavior when asking for a rangeValue with a given non-existant domainValue :
+     * If unknown is null : add domainValue to the domain, then return a rangeValue (= scale implicit).
+     * If unknown is not null : return unknown.
+     */
+    var unknown: R? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    /**
+     * If domain is specified, sets the domain to the specified array of values.
+     * The first element in domain will be mapped to the first element in the range, the second domain value to
+     * the second range value, and so on.
+     * Domain values are stored internally in a map from stringified value to index; the resulting index is then used
+     * to retrieve a value from the range. Thus, an ordinal scale’s values must be coercible to a string,
+     * and the stringified version of the domain value uniquely identifies the corresponding range value.
+     *
+     * Setting the domain on an ordinal scale is optional if the unknown value is implicit (the default).
+     * In this case, the domain will be inferred implicitly from usage by assigning each unique value passed
+     * to the scale a new value from the range.
+     * Note that an explicit domain is recommended to ensure deterministic behavior, as inferring the domain
+     * from usage will be dependent on ordering.
+     */
+    override var domain: List<D>
+        get() = innerDomain.toList()
+        set(value) {
+            innerDomain.clear()
+            index.clear()
+            value.forEach {
+                if (!index.containsKey(it)) {
+                    innerDomain.add(it)
+                    index.put(it, domain.size - 1)
+                }
+            }
+        }
+
+    /**
+     * If range is specified, sets the range of the ordinal scale to the specified array of values.
+     * The first element in the domain will be mapped to the first element in range, the second domain value to
+     * the second range value, and so on.
+     * If there are fewer elements in the range than in the domain, the scale will reuse values from the start
+     * of the range.
+     */
+    override var range: List<R> = arrayListOf()
+        get() = field.toList()
+        set(value) {
+            require(value.isNotEmpty(), { "Range can't be empty." })
+            field = value.toList()
+        }
+
+    override operator fun invoke(domainValue: D): R {
+        if (unknown == null && !index.containsKey(domainValue)) {
+            innerDomain.add(domainValue)
+            index.put(domainValue, domain.size - 1)
+        }
+
+        val index = index[domainValue] ?: return unknown ?: throw IllegalStateException()
+        return when {
+            range.isEmpty() -> unknown ?: throw IllegalStateException()
+            else -> range[index % range.size]
+        }
+    }
+
+    override fun ticks(count: Int): List<D> = listOf()
+}
+
+fun <D, R> ordinalScale() = OrdinalScale<D, R>()
---
+++ scale/d2v-scale-common/src/test/kotlin/io/data2viz/scale/ScaleOrdinalTests.kt
@@ -0,0 +1,184 @@
+package io.data2viz.scale
+
+import io.data2viz.test.TestBase
+import io.data2viz.test.shouldThrow
+import kotlin.test.Test
+
+class ScaleOrdinalTests : TestBase() {
+
+    val epsilon = 1e6
+
+    @Test
+    fun ordinal_has_expected_defaults_LEGACY() {
+        val scale = ordinalScale<Int, Int>()
+
+        scale.domain shouldBe listOf()
+        scale.range shouldBe listOf()
+        shouldThrow<IllegalStateException> { scale(0) }
+        scale.domain shouldBe listOf(0)
+    }
+
+    @Test
+    fun ordinal_x_maps_unique_x_in_domain_to_corresponding_value_in_range_LEGACY() {
+        val scale = ordinalScale<Int, String>()
+
+        scale.domain = listOf(0, 1)
+        scale.range = listOf("foo", "bar")
+        scale(0) shouldBe "foo"
+        scale(1) shouldBe "bar"
+
+        scale.range = listOf("a", "b", "c")
+        scale(0) shouldBe "a"
+        scale(1) shouldBe "b"
+    }
+
+    @Test
+    fun ordinal_x_implicitly_extends_the_domain_when_a_range_is_explicitly_specified_LEGACY() {
+        val scale = ordinalScale<Int, String>()
+        scale.range = listOf("foo", "bar")
+
+        scale.domain shouldBe listOf()
+        scale(0) shouldBe "foo"
+        scale.domain shouldBe listOf(0)
+        scale(1) shouldBe "bar"
+        scale.domain shouldBe listOf(0, 1)
+        scale(0) shouldBe "foo"
+        scale.domain shouldBe listOf(0, 1)
+    }
+
+    @Test
+    fun ordinal_domain_makes_a_copy_of_the_domain_LEGACY() {
+        val scale = ordinalScale<String, String>()
+
+        val domain = arrayListOf("red", "green")
+        scale.domain = domain
+        domain.add("blue")
+        scale.domain shouldBe listOf("red", "green")
+    }
+
+    @Test
+    fun ordinal_x_then_domain_makes_a_copy_of_the_domain_LEGACY() {
+        val scale = ordinalScale<String, String>()
+        scale.range = listOf("toto")
+
+        scale.domain = arrayListOf("red", "green")
+        val domain = scale.domain
+        scale("blue")
+        scale.domain shouldBe listOf("red", "green", "blue")
+        domain shouldBe listOf("red", "green")
+    }
+
+    @Test
+    fun ordinal_domain_replaces_previous_values_LEGACY() {
+        val scale = ordinalScale<Int, String>()
+        scale.range = listOf("foo", "bar")
+
+        scale(1) shouldBe "foo"
+        scale(0) shouldBe "bar"
+        scale.domain shouldBe listOf(1, 0)
+
+        scale.domain = listOf(0, 1)
+        scale(0) shouldBe "foo"
+        scale(1) shouldBe "bar"
+        scale.domain shouldBe listOf(0, 1)
+    }
+
+    @Test
+    fun ordinal_domain_is_ordered_by_appearance_LEGACY() {
+        val scale = ordinalScale<String, String>()
+        scale.range = listOf("toto")
+
+        scale("foo")
+        scale("bar")
+        scale("baz")
+        scale.domain shouldBe listOf("foo", "bar", "baz")
+
+        scale.domain = listOf("bar", "baz")
+        scale("foo")
+        scale.domain shouldBe listOf("bar", "baz", "foo")
+
+        scale.domain = listOf("baz", "foo")
+        scale.domain shouldBe listOf("baz", "foo")
+
+        scale.domain = listOf()
+        scale("foo")
+        scale("bar")
+        scale.domain shouldBe listOf("foo", "bar")
+    }
+
+    @Test
+    fun ordinal_range_makes_a_copy_of_the_range_LEGACY() {
+        val scale = ordinalScale<String, String>()
+
+        val range = arrayListOf("red", "green")
+        scale.range = range
+        range.add("blue")
+        scale.range shouldBe listOf("red", "green")
+    }
+
+    @Test
+    fun ordinal_x_then_range_makes_a_copy_of_the_range_LEGACY() {
+        val scale = ordinalScale<String, String>()
+
+        scale.range = arrayListOf("red", "green")
+        val range = scale.range
+        scale.range = arrayListOf("red", "green", "blue")
+        scale.range shouldBe listOf("red", "green", "blue")
+        range shouldBe listOf("red", "green")
+    }
+
+    @Test
+    fun ordinal_range_values_does_not_discard_implicit_domain_associations_LEGACY() {
+        val scale = ordinalScale<Int, String>()
+
+        scale.range = listOf("toto")
+        scale(0) shouldBe ("toto")
+        scale(1) shouldBe ("toto")
+
+        scale.range = listOf("red", "green")
+        scale(0) shouldBe ("red")
+        scale(1) shouldBe ("green")
+    }
+
+    @Test
+    fun ordinal_value_recylces_values_when_exhausted_LEGACY() {
+        val scale = ordinalScale<Int, String>()
+        scale.range = listOf("a", "b", "c")
+
+        scale(0) shouldBe ("a")
+        scale(1) shouldBe ("b")
+        scale(2) shouldBe ("c")
+        scale(3) shouldBe ("a")
+        scale(4) shouldBe ("b")
+        scale(5) shouldBe ("c")
+        scale(2) shouldBe ("c")
+        scale(1) shouldBe ("b")
+        scale(0) shouldBe ("a")
+    }
+
+    @Test
+    fun ordinal_unknown_x_sets_output_value_for_unknown_inputs_LEGACY() {
+        val scale = ordinalScale<Int, String>()
+        scale.domain = listOf(1, 2)
+        scale.range = listOf("foo", "bar")
+        scale.unknown = "gray"
+
+        scale(1) shouldBe "foo"
+        scale(2) shouldBe "bar"
+        scale(3) shouldBe "gray"
+        scale(-6) shouldBe "gray"
+    }
+
+    @Test
+    fun ordinal_unknown_x_prevents_domain_extension_if_x_not_implicit_LEGACY() {
+        val scale = ordinalScale<Int, String>()
+        scale.domain = listOf(1, 2)
+        scale.range = listOf("foo", "bar")
+        scale.unknown = "undefined"
+
+        scale(1) shouldBe "foo"
+        scale(2) shouldBe "bar"
+        scale(3) shouldBe "undefined"
+        scale.domain shouldBe listOf(1, 2)
+    }
+}
