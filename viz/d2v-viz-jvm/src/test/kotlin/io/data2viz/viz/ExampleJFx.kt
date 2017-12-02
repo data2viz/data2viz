aa9cb724babcaf9c6ecfe19e1f2b162369bba627
gzoritchak <g.zoritchak@gmail.com>
2017-12-02 03:14:49 +0100

    Using ?.let instead of !!

--- viz/d2v-viz-jvm/src/test/kotlin/io/data2viz/viz/ExampleJFx.kt
+++ viz/d2v-viz-jvm/src/test/kotlin/io/data2viz/viz/ExampleJFx.kt
@@ -3,14 +3,12 @@ package io.data2viz.viz
 import javafx.application.Application
 import javafx.scene.Group
 import javafx.scene.Scene
-import javafx.scene.web.WebView
 import javafx.stage.Stage
 
 
 fun main(args: Array<String>) {
     Application.launch(HelloApp::class.java)
 }
-
 
 val data = listOf(
         Domain(10.0, 10.0),
@@ -20,13 +18,16 @@ val data = listOf(
 
 
 class HelloApp : Application() {
+
     override fun start(primaryStage: Stage?) {
         val root = Group()
         root.viz {
             commonViz(data)
         }
 
-        primaryStage!!.scene = (Scene(root, 500.0, 500.0))
-        primaryStage.show()
+        primaryStage?.let {
+            it.scene = (Scene(root, 500.0, 500.0))
+            it.show()
+        }
     }
 }
