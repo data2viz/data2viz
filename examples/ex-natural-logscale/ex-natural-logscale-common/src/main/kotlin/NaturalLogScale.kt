eab281857be93d45906e4239e6c7bf44f3435d63
gzoritchak <g.zoritchak@gmail.com>
2017-12-13 01:01:38 +0100

    VIZ: rename Margin -> Margins

--- examples/ex-natural-logscale/ex-natural-logscale-common/src/main/kotlin/NaturalLogScale.kt
+++ examples/ex-natural-logscale/ex-natural-logscale-common/src/main/kotlin/NaturalLogScale.kt
@@ -10,7 +10,7 @@ import kotlin.math.round
 
 var superscript = "⁰¹²³⁴⁵⁶⁷⁸⁹"
 
-val margins = Margin(40.5, 40.5, 50.5, 60.5)
+val margins = Margins(40.5, 40.5, 50.5, 60.5)
 
 val width = 960.0 - margins.hMargins
 val height = 500.0 - margins.vMargins
