588cc520546a1eb3a5c429b4ac3f1d9f66ab3be1
gzoritchak <g.zoritchak@gmail.com>
2017-12-14 13:43:41 +0100

    EXAMPLE d2v-progression: adding legend, and separation between versions.

--- examples/ex-progression-stack/ex-progression-stack-common/src/main/kotlin/io/data2viz/examples/progression/D2vProgressionStackCommon.kt
+++ examples/ex-progression-stack/ex-progression-stack-common/src/main/kotlin/io/data2viz/examples/progression/D2vProgressionStackCommon.kt
@@ -1,22 +1,28 @@
 package io.data2viz.examples.progression
 
-import io.data2viz.axis.Orient
-import io.data2viz.axis.axis
-import io.data2viz.color.color
-import io.data2viz.viz.VizContext
+import io.data2viz.axis.*
+import io.data2viz.color.*
 import io.data2viz.scale.*
-import io.data2viz.viz.Margins
+import io.data2viz.viz.*
 
+val legendLineHeight = 30.0
 
-val margins = Margins(50.0)
+val margins = Margins(50.0, bottom =  4 * legendLineHeight + 40.0)
 
 val width = 1600.0 - margins.hMargins
-val height = 500.0 - margins.vMargins
+val height = 450.0
 
-val Progression.totalLOC: Int
+//Colors from http://colorbrewer2.org/#type=diverging&scheme=RdBu&n=7
+val colorExpected = "#dddddd".color
+val colorTest = "#b2182b".color
+val colorCommon = "#2166ac".color
+val colorJs = "#67a9cf".color
+val colorJVM = "#d1e5f0".color
+
+val ModuleState.totalLOC: Int
     get() = commonLOC + jsLOC + JVMLOC
 
-fun checkData(data: List<Progression>) {
+fun checkData(data: List<ModuleState>) {
     data.forEach {
         check(it.estimatedLOC >= it.totalLOC) { "Estimated LOC under total LOC (${it.estimatedLOC} > ${it.totalLOC}" }
     }
@@ -40,33 +46,47 @@ fun VizContext.progression() {
 
     val y0 = yScale(maxEstimated)
 
-    val moduleNames = modules.map { it.module }
+    val moduleNames = modules.map { it.name }
 
     val xScale = scales.band(moduleNames) {
         range = intervalOf(.0, width)
-        paddingInner = .1
-        paddingOuter = .3
+        padding = .1
     }
+
+    legend()
 
     modules.forEach { progression ->
 
         // tests LOC rectangle
         rect {
-            fill = "#d6604d".color
-            x = xScale(progression.module)
+            fill = colorTest
+            stroke = null
+            x = xScale(progression.name)
             width = xScale.bandwidth
             y = y0
             height = yScale(progression.testsLOC.toDouble())
         }
 
+        val expectedLocHeight = yScale(progression.estimatedLOC.toDouble())
         val commonLocHeight = yScale(progression.commonLOC.toDouble())
         val jvmLocHeight = yScale(progression.JVMLOC.toDouble())
         val jsLocHeight = yScale(progression.jsLOC.toDouble())
 
+        // Expected LOC rectangle
+        rect {
+            fill = colorExpected
+            stroke = null
+            x = xScale(progression.name)
+            width = xScale.bandwidth
+            y = y0 - expectedLocHeight
+            this.height = expectedLocHeight
+        }
+
         // common LOC rectangle
         rect {
-            fill = "#2166ac".color
-            x = xScale(progression.module)
+            fill = colorCommon
+            stroke = null
+            x = xScale(progression.name)
             width = xScale.bandwidth
             y = y0 - commonLocHeight
             this.height = commonLocHeight
@@ -74,22 +94,23 @@ fun VizContext.progression() {
 
         // Js LOC rectangle
         rect {
-            fill = "#4393c3".color
-            x = xScale(progression.module)
+            fill = colorJs
+            stroke = null
+            x = xScale(progression.name)
             width = xScale.bandwidth
             y = y0 - commonLocHeight - jsLocHeight
             this.height = jsLocHeight
         }
-        
-        // common LOC rectangle
+
+        // JVM LOC rectangle
         rect {
-            fill = "#92c5de".color
-            x = xScale(progression.module)
+            fill = colorJVM
+            stroke = null
+            x = xScale(progression.name)
             width = xScale.bandwidth
             y = y0 - commonLocHeight - jsLocHeight - jvmLocHeight
             this.height = jvmLocHeight
         }
-
     }
 
     group {
@@ -99,4 +120,63 @@ fun VizContext.progression() {
         axis(Orient.BOTTOM, xScale)
     }
 
+    //module vertical separations
+    val modulesByVersion = modules
+            .groupBy { it.version }
+
+    val versions = modulesByVersion.keys.sorted()
+    var xSeparation = xScale.paddingInner * xScale.step / 2
+    (0 until versions.size).forEach {
+
+        val key = versions[it]
+        val packageCountForVersion = modulesByVersion.get(key)?.size ?: 0
+        println("Version $key :: $packageCountForVersion packages")
+        val versionWidth = xScale.step * packageCountForVersion
+
+        text {
+            anchor = TextAnchor.MIDDLE
+            transform {
+                translate(x = xSeparation + .5 * versionWidth)
+            }
+            textContent = "version $key"
+        }
+        xSeparation += versionWidth
+
+        if (it != versions.size - 1)
+            group {
+                transform { translate(x = xSeparation) }
+                line {
+                    y1 = .0
+                    y2 = height
+                }
+            }
+    }
 }
+
+private fun VizContext.legend() {
+    group {
+        transform { translate(y = height) }
+        colorLegend(colorJVM, "Current kotlin jvm LOC", line = 0)
+        colorLegend(colorJs, "Current kotlin js LOC", line = 1)
+        colorLegend(colorCommon, "Current kotlin common LOC", line = 2)
+        colorLegend(colorTest, "Current total test LOC", line = 3)
+    }
+}
+
+private fun ParentItem.colorLegend(color: Color, legend: String, line: Int = 0) =
+    group {
+        transform {
+            translate(y = line * legendLineHeight)
+        }
+        rect {
+            fill = color
+            stroke = colors.black
+            height = 20.0
+            width = 20.0
+        }
+
+        text {
+            transform { translate(x = 25.0, y = 15.0) }
+            textContent = legend
+        }
+    }
--- examples/ex-progression-stack/ex-progression-stack-common/src/main/kotlin/io/data2viz/examples/progression/Data.kt
+++ examples/ex-progression-stack/ex-progression-stack-common/src/main/kotlin/io/data2viz/examples/progression/Data.kt
@@ -1,43 +1,40 @@
 package io.data2viz.examples.progression
 
 
-data class Progression(
-        val version: String, 
-        val module: String, 
-        val estimatedLOC: Int, 
-        val commonLOC: Int, 
-        val jsLOC: Int, 
-        val JVMLOC: Int, 
+data class ModuleState(
+        val version: String,
+        val name: String,
+        val estimatedLOC: Int,
+        val commonLOC: Int,
+        val jsLOC: Int,
+        val JVMLOC: Int,
         val testsLOC: Int)
 
 
-val modules = listOf(
-        Progression("0.2",  "core", 150, 76, 3, 0, 150),
-        Progression("0.2",  "voronoi", 1000, 930, 0, 0, 550),
-        Progression("0.2",  "color", 400, 378, 2, 6, 307),
-        Progression("0.2",  "tests", 296, 269, 19, 8, 0),
-        Progression("0.2",  " ", 0, 0, 0, 0, 0),
-        Progression("0.3",  "path", 156, 139, 14, 3, 450),
-        Progression("0.3",  "shape", 1902, 1871, 0, 31, 1300),
-        Progression("0.3",  "scale", 753, 715, 0, 0, 1653),
-        Progression("0.3",  "format", 781, 732, 7, 42, 1025),
-        Progression("0.3",  "viz", 400, 52, 149, 121, 85),
-        Progression("0.3",  "interpolate", 462, 206, 13, 0, 300),
-        Progression("0.3",  "random", 51, 46, 3, 2, 0),
-        Progression("0.3",  "axis", 142, 0, 39, 0, 31),
-        Progression("0.3",  "timeformat", 574, 0, 0, 0, 0),
-        Progression("0.3",  "time", 298, 0, 0, 0, 0),
-        Progression("0.3",  "  ", 0, 0, 0, 0, 0),
-        Progression("0.4",  "contour", 350, 230, 26, 0, 364),
-        Progression("0.4",  "sankey", 265, 0, 0, 0, 0),
-        Progression("0.4",  "chord", 179, 0, 0, 0, 0),
-        Progression("0.4",  "   ", 0, 0, 0, 0, 0),
-        Progression("0.5",  "hierarchy", 1100, 0, 0, 0, 0),
-        Progression("0.5",  "vizchart", 1200, 0, 0, 0, 0),
-        Progression("0.5",  "scalechromatic", 386, 0, 0, 0, 0)
-        )
+val modules = listOf (
+        ModuleState("0.2",  "core", 150, 76, 3, 0, 150),
+        ModuleState("0.2",  "voronoi", 1000, 930, 0, 0, 550),
+        ModuleState("0.2",  "color", 400, 378, 2, 6, 307),
+        ModuleState("0.2",  "tests", 296, 269, 19, 8, 0),
+        ModuleState("0.3",  "path", 156, 139, 14, 3, 450),
+        ModuleState("0.3",  "shape", 1902, 1871, 0, 31, 1300),
+        ModuleState("0.3",  "scale", 753, 715, 0, 0, 1653),
+        ModuleState("0.3",  "format", 781, 732, 7, 42, 1025),
+        ModuleState("0.3",  "viz", 400, 52, 149, 121, 85),
+        ModuleState("0.3",  "interpolate", 462, 206, 13, 0, 300),
+        ModuleState("0.3",  "random", 51, 46, 3, 2, 0),
+        ModuleState("0.3",  "axis", 142, 0, 39, 0, 31),
+        ModuleState("0.3",  "time", 298, 0, 0, 0, 0),
+        ModuleState("0.3",  "timeformat", 574, 0, 0, 0, 0),
+        ModuleState("0.4",  "contour", 350, 230, 26, 0, 364),
+        ModuleState("0.4",  "sankey", 265, 0, 0, 0, 0),
+        ModuleState("0.4",  "chord", 179, 0, 0, 0, 0),
+        ModuleState("0.5",  "hierarchy", 1100, 0, 0, 0, 0),
+        ModuleState("0.5",  "vizchart", 1200, 0, 0, 0, 0),
+        ModuleState("0.5",  "scale\nchromatic", 386, 0, 0, 0, 0)
+)
 
 
-val Progression.remainingLOC:Int
+val ModuleState.remainingLOC:Int
         get() = estimatedLOC - commonLOC - jsLOC - JVMLOC
 
