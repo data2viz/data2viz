3dbf662b5f19e6e45213be5fa9b7e47220be1458
gzoritchak <g.zoritchak@gmail.com>
2017-12-14 13:44:02 +0100

    EXAMPLE d2v-progression: kotlin-js version.

---
+++ examples/ex-progression-stack/ex-progression-stack-js/src/main/kotlin/D2vProgressionStackJs.kt
@@ -0,0 +1,20 @@
+package io.data2viz.examples.progression
+
+import io.data2viz.viz.selectOrCreateSvg
+import io.data2viz.viz.viz
+
+
+@Suppress("unused")
+
+fun main(args: Array<String>) {
+
+    val root = selectOrCreateSvg().apply {
+        setAttribute("width", "${width + margins.hMargins}")
+        setAttribute("height", "${height + margins.vMargins}")
+    }
+
+    root.viz {
+        progression()
+    }
+
+}
