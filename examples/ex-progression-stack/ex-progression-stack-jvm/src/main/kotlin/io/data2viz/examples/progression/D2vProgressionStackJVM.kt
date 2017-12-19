0c7da02aec85024e8039c1bc960d121e026b604c
gzoritchak <g.zoritchak@gmail.com>
2017-12-19 23:34:28 +0100

    EXAMPLE progression stack: sout total common, jvm, js.

--- examples/ex-progression-stack/ex-progression-stack-jvm/src/main/kotlin/io/data2viz/examples/progression/D2vProgressionStackJVM.kt
+++ examples/ex-progression-stack/ex-progression-stack-jvm/src/main/kotlin/io/data2viz/examples/progression/D2vProgressionStackJVM.kt
@@ -13,6 +13,15 @@ class D2vProgressionStackJVM : Application() {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            
+            val totalCommon = modules.fold(0) {acc: Int, moduleState: ModuleState -> acc +moduleState.commonLOC}
+            val totalJs = modules.fold(0) {acc: Int, moduleState: ModuleState -> acc +moduleState.jsLOC}
+            val totalJFX = modules.fold(0) {acc: Int, moduleState: ModuleState -> acc +moduleState.JVMLOC}
+
+            println("Total common:: $totalCommon")
+            println("Total JS    :: $totalJs")
+            println("Total JFX   :: $totalJFX")
+            
             Application.launch(D2vProgressionStackJVM::class.java)
         }
     }
