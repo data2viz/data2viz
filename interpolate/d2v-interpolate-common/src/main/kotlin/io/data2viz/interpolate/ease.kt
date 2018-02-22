e56dc9f7877d86b9ce4585ecee42b137c9e427e4
pierre mariac <pmariac@gmail.com>
2018-02-22 11:26:12 +0100

    CORE : added some more trigonometry constants (TAU, quarterPI...)

--- interpolate/d2v-interpolate-common/src/main/kotlin/io/data2viz/interpolate/ease.kt
+++ interpolate/d2v-interpolate-common/src/main/kotlin/io/data2viz/interpolate/ease.kt
@@ -1,6 +1,6 @@
 package io.data2viz.interpolate
 
-import io.data2viz.math.halfPI
+import io.data2viz.math.HALFPI_ANGLE
 import kotlin.math.cos
 import kotlin.math.pow
 import kotlin.math.sqrt
@@ -13,7 +13,7 @@ fun cubicOut(t:Double) = (t-1)*(t-1)*(t-1) + 1
 fun cubicInOut(t:Double) =  (if(t <= .5) (8*t*t*t) else ((2*t-2)*(2*t-2)*(2*t-2)+2))/2
 
 
-fun sin(x:Double) = 1 - cos(x * halfPI.rad)
+fun sin(x:Double) = 1 - cos(x * HALFPI_ANGLE.rad)
 fun poly(e:Double) = {t:Double -> t.pow(e)}
 
 
