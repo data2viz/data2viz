33e0ce47ea25c25f8ecd04c924c59f066adf6e37
pierre mariac <pmariac@gmail.com>
2018-03-02 11:39:45 +0100

    GEO : added more clipping tests

--- geo/d2v-geo-common/src/test/kotlin/io/data2viz/geo/projection/RectangleClippingTests.kt
+++ geo/d2v-geo-common/src/test/kotlin/io/data2viz/geo/projection/RectangleClippingTests.kt
@@ -9,7 +9,7 @@ import io.data2viz.test.TestBase
 import kotlin.math.PI
 import kotlin.test.Test
 
-class VariousClippingTests : TestBase() {
+class RectangleClippingTests : TestBase() {
 
     fun getProjection() = equirectangularProjection {
         translate = doubleArrayOf(480.0, 350.0)
@@ -51,5 +51,38 @@ class VariousClippingTests : TestBase() {
         val path: SvgPath = geoPath.path(polygon) as SvgPath
 
         path.path.round() shouldBe "M498,332L480,350L305.46707480056705,350L305.46707480056705,350L218.20061220085057,262.7335374002835L305.46707480056705,175.46707480056705L480,175.46707480056705L498,193.46707480056705L498,332Z".round()
+    }
+
+    @Test
+    fun equirectangular_rectangle_clipping_north() {
+        val projection = getProjection()
+        projection.postClip = clipRectangle(Extent(200.0, 200.0, 700.0, 500.0))
+
+        val geoPath = geoPath(projection, svgPath())
+        val path: SvgPath = geoPath.path(polygon) as SvgPath
+
+        path.path.round() shouldBe "M504.53292519943295,200L567.2664625997165,262.7335374002835L480,350L305.46707480056705,350L305.46707480056705,350L218.20061220085057,262.7335374002835L280.9341496011341,200L504.53292519943295,200Z".round()
+    }
+
+    @Test
+    fun equirectangular_rectangle_clipping_south() {
+        val projection = getProjection()
+        projection.postClip = clipRectangle(Extent(48.0, 50.0, 700.0, 300.0))
+
+        val geoPath = geoPath(projection, svgPath())
+        val path: SvgPath = geoPath.path(polygon) as SvgPath
+
+        path.path.round() shouldBe "M255.46707480056705,300L218.20061220085057,262.7335374002835L305.46707480056705,175.46707480056705L480,175.46707480056705L567.2664625997165,262.7335374002835L530,300L255.46707480056705,300Z".round()
+    }
+
+    @Test
+    fun equirectangular_rectangle_clipping_west() {
+        val projection = getProjection()
+        projection.postClip = clipRectangle(Extent(250.0, 50.0, 700.0, 500.0))
+
+        val geoPath = geoPath(projection, svgPath())
+        val path: SvgPath = geoPath.path(polygon) as SvgPath
+
+        path.path.round() shouldBe "M250,230.9341496011341L305.46707480056705,175.46707480056705L480,175.46707480056705L567.2664625997165,262.7335374002835L480,350L305.46707480056705,350L305.46707480056705,350L250,294.53292519943295L250,230.9341496011341Z".round()
     }
 }
