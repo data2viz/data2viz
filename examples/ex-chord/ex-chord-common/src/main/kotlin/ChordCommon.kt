d21b84a64cba4700575e1f826b623a500a05209a
gzoritchak <g.zoritchak@gmail.com>
2018-03-05 20:31:34 +0100

    From busses to avengers.

--- examples/ex-chord/ex-chord-common/src/main/kotlin/ChordCommon.kt
+++ examples/ex-chord/ex-chord-common/src/main/kotlin/ChordCommon.kt
@@ -3,40 +3,67 @@ package io.data2viz.examples.chord
 import io.data2viz.chord.ChordGroup
 import io.data2viz.core.random
 import io.data2viz.chord.ChordLayout
+import io.data2viz.chord.Chords
+import io.data2viz.color.Color
 import io.data2viz.color.colors
 import io.data2viz.shape.arc
 import io.data2viz.shape.const
 import io.data2viz.viz.VizContext
 
 
-class CityBusses(val cityName: String) {
-    private val toCity = mutableMapOf<CityBusses, Double>()
-    fun trafficTo(otherCity: CityBusses) =  if (otherCity == this) .0 else toCity.getOrPut(otherCity) { 1000 * random() }
-}
+class Film(val name:String, val avengers:List<Avenger>)
+
+class Avenger(val name:String)
+
+val blackWidow = Avenger("Black Widow")
+val captainAmerica = Avenger("Captain America")
+val theHulk = Avenger("the Hulk")
+val ironMan = Avenger("Iron Man")
+val thor = Avenger("Thor")
+val hawkeye = Avenger("hawkeye")
+
+val filmIronMan1        = Film("Iron Man 1", listOf(ironMan))
+val filmIronMan2        = Film("Iron Man 2", listOf(ironMan,blackWidow))
+val filmIronMan3        = Film("Iron Man 3", listOf(ironMan, theHulk))
+val filmAvengers1       = Film("Avengers", listOf(ironMan, captainAmerica, theHulk, thor, hawkeye, blackWidow))
+val filmAvengers2       = Film("Avengers, L'ère d'Ultron", listOf(ironMan, captainAmerica, theHulk, thor, hawkeye, blackWidow))
+val filmAvengers3       = Film("Avengers, Infinity War", listOf(ironMan, captainAmerica, theHulk, hawkeye, blackWidow))
+val filmThor            = Film("Thor", listOf(thor, hawkeye))
+val filmThor2           = Film("Thor, le monde des ténèbres", listOf(thor, captainAmerica))
+val filmThor3           = Film("Thor, Ragnakok", listOf(thor))
+val filmCaptainAmerica1 = Film("Captain America, First Avenger", listOf(captainAmerica))
+val filmCaptainAmerica2 = Film("Captain America, Le Soldat de l'hiver", listOf(captainAmerica, blackWidow))
+val filmCaptainAmerica3 = Film("Captain America, Civil War", listOf(captainAmerica, ironMan, hawkeye, blackWidow))
 
 
-val busses = listOf(
-    CityBusses("Paris"),
-    CityBusses("Madrid"),
-    CityBusses("Rome"),
-    CityBusses("Bern"),
-    CityBusses("Geneva")
-)
+val films = listOf(
+    filmAvengers1, filmAvengers2, filmAvengers3,
+    filmCaptainAmerica1, filmCaptainAmerica2, filmCaptainAmerica3,
+    filmIronMan1, filmIronMan2, filmIronMan3,
+    filmThor, filmThor2, filmThor3
+    )
 
+val avengers    = listOf (blackWidow, captainAmerica, hawkeye, theHulk, ironMan, thor)
+val colors      = listOf (0x301E1E, 0x083E77, 0x342350, 0x567235, 0x8B161C, 0xDF7C00).map { Color(it) }
+
+val Avenger.color: Color
+        get() = io.data2viz.examples.chord.colors[avengers.indexOf(this)]
 
 val width = 600.0
 val height = width
 val outer = minOf(width, height) * 0.5 - 40.0
 val inner = outer - 30
 
-val chord = ChordLayout<CityBusses>().apply {
-    padAngle = .05
+val chord = ChordLayout<Avenger>().apply {
+    padAngle = .15
 }
 
-val chords = chord.chord(busses, { a, b -> a.trafficTo(b) })
+fun collaborations(avengers:List<Avenger>) = films.filter { it.avengers.containsAll(avengers) }.size.toDouble()
+
+val chords: Chords = chord.chord(avengers, { a, b -> if (a==b) .0 else collaborations(listOf(a,b))})
 
 val arc = arc<ChordGroup> {
-    innerRadius = const(inner)
+    innerRadius = const(inner + 3)
     outerRadius = const(outer)
     startAngle = { it.startAngle }
     endAngle = { it.endAngle }
@@ -48,17 +75,19 @@ val ribbon = io.data2viz.chord.ribbon(inner)
 fun VizContext.chordViz() {
     transform { translate(width / 2, height / 2) }
 
-    chords.groups.forEach {
+    chords.groups.forEachIndexed { index, it ->
         path {
-            fill = colors.steelblue
+            fill = io.data2viz.examples.chord.colors[index]
+            stroke = null
             arc.arc(it, this)
         }
     }
 
-    chords.chords.forEach {
+    chords.chords.forEach { chord ->
         path {
-            fill = colors.red.apply { alpha = .6 }
-            ribbon(it, this)
+            fill = io.data2viz.examples.chord.colors[chord.source.index].apply { alpha = .8 }
+            stroke = null
+            ribbon(chord, this)
         }
     }
 }
