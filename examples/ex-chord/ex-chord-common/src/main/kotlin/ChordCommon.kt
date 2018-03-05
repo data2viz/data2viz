97dfb316d508a7488ea917a240476364ac3ce6b4
gzoritchak <g.zoritchak@gmail.com>
2018-03-06 00:18:08 +0100

    Updating Chord sample (DV-101).

--- examples/ex-chord/ex-chord-common/src/main/kotlin/ChordCommon.kt
+++ examples/ex-chord/ex-chord-common/src/main/kotlin/ChordCommon.kt
@@ -5,12 +5,10 @@ import kotlin.math.*
 
 import io.data2viz.chord.Chord
 import io.data2viz.chord.ChordGroup
-import io.data2viz.core.random
 import io.data2viz.chord.ChordLayout
 import io.data2viz.chord.Chords
 import io.data2viz.color.Color
 import io.data2viz.color.LinearGradient
-import io.data2viz.color.colors
 import io.data2viz.shape.arc
 import io.data2viz.shape.const
 import io.data2viz.viz.VizContext
@@ -20,31 +18,27 @@ class Film(val name:String, val avengers:List<Avenger>)
 
 class Avenger(val name:String)
 
-val blackWidow = Avenger("Black Widow")
-val captainAmerica = Avenger("Captain America")
-val theHulk = Avenger("the Hulk")
-val ironMan = Avenger("Iron Man")
-val thor = Avenger("Thor")
-val hawkeye = Avenger("hawkeye")
+val blackWidow      = Avenger("Black Widow")
+val captainAmerica  = Avenger("Captain America")
+val theHulk         = Avenger("the Hulk")
+val ironMan         = Avenger("Iron Man")
+val thor            = Avenger("Thor")
+val hawkeye         = Avenger("hawkeye")
 
-val filmIronMan1        = Film("Iron Man 1", listOf(ironMan))
-val filmIronMan2        = Film("Iron Man 2", listOf(ironMan,blackWidow))
-val filmIronMan3        = Film("Iron Man 3", listOf(ironMan, theHulk))
-val filmAvengers1       = Film("Avengers", listOf(ironMan, captainAmerica, theHulk, thor, hawkeye, blackWidow))
-val filmAvengers2       = Film("Avengers, L'ère d'Ultron", listOf(ironMan, captainAmerica, theHulk, thor, hawkeye, blackWidow))
-val filmAvengers3       = Film("Avengers, Infinity War", listOf(ironMan, captainAmerica, theHulk, hawkeye, blackWidow))
-val filmThor            = Film("Thor", listOf(thor, hawkeye))
-val filmThor2           = Film("Thor, le monde des ténèbres", listOf(thor, captainAmerica))
-val filmThor3           = Film("Thor, Ragnakok", listOf(thor))
-val filmCaptainAmerica1 = Film("Captain America, First Avenger", listOf(captainAmerica))
-val filmCaptainAmerica2 = Film("Captain America, Le Soldat de l'hiver", listOf(captainAmerica, blackWidow))
-val filmCaptainAmerica3 = Film("Captain America, Civil War", listOf(captainAmerica, ironMan, hawkeye, blackWidow))
-val films = listOf(
-    filmAvengers1, filmAvengers2, filmAvengers3,
-    filmCaptainAmerica1, filmCaptainAmerica2, filmCaptainAmerica3,
-    filmIronMan1, filmIronMan2, filmIronMan3,
-    filmThor, filmThor2, filmThor3
-    )
+val films = listOf (
+    Film("Avengers", listOf(ironMan, captainAmerica, theHulk, thor, hawkeye, blackWidow)),
+    Film("Avengers, L'ère d'Ultron", listOf(ironMan, captainAmerica, theHulk, thor, hawkeye, blackWidow)),
+    Film("Avengers, Infinity War", listOf(ironMan, captainAmerica, theHulk, hawkeye, blackWidow)),
+    Film("Captain America, First Avenger", listOf(captainAmerica)),
+    Film("Captain America, Le Soldat de l'hiver", listOf(captainAmerica, blackWidow)),
+    Film("Captain America, Civil War", listOf(captainAmerica, ironMan, hawkeye, blackWidow)),
+    Film("Iron Man 1", listOf(ironMan)),
+    Film("Iron Man 2", listOf(ironMan,blackWidow)), 
+    Film("Iron Man 3", listOf(ironMan, theHulk)),
+    Film("Thor", listOf(thor, hawkeye)),
+    Film("Thor, le monde des ténèbres", listOf(thor, captainAmerica)), 
+    Film("Thor, Ragnakok", listOf(thor))
+)
 
 val avengers    = listOf (blackWidow,   captainAmerica, hawkeye,    theHulk,    ironMan,    thor)
 val colors      = listOf (0x301E1E,     0x083E77,       0x342350,   0x567235,   0x8B161C,   0xDF7C00).map { Color(it) }
