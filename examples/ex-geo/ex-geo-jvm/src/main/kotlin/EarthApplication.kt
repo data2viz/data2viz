4f7006b54018b8797efef35e827385283c9950e2
pierre mariac <pmariac@gmail.com>
2018-03-16 16:44:11 +0100

    EXAMPLE : added auto-rotation to EX-GEO

--- examples/ex-geo/ex-geo-jvm/src/main/kotlin/EarthApplication.kt
+++ examples/ex-geo/ex-geo-jvm/src/main/kotlin/EarthApplication.kt
@@ -1,10 +1,14 @@
 package io.data2viz.examples.geo
 
 import io.data2viz.color.colors
+import io.data2viz.geo.path.GeoPath
 import io.data2viz.geo.path.geoPath
 import io.data2viz.geo.projection.Extent
 import io.data2viz.geo.projection.orthographic
+import io.data2viz.geojson.GeoJsonObject
 import io.data2viz.geojson.toGeoJsonObject
+import io.data2viz.timer.timer
+import io.data2viz.viz.PathVizElement
 import io.data2viz.viz.PathVizJfx
 import io.data2viz.viz.newPath
 import javafx.application.Application
@@ -13,6 +17,12 @@ import javafx.scene.layout.Pane
 import javafx.stage.Stage
 
 class EarthApplication : Application() {
+
+    lateinit var geoPathInner: GeoPath
+    lateinit var geoPathOuter: GeoPath
+    lateinit var pathVizJfxInner: PathVizElement
+    lateinit var pathOuter: PathVizElement
+    lateinit var world: GeoJsonObject
 
     companion object {
         @JvmStatic
@@ -24,8 +34,8 @@ class EarthApplication : Application() {
     override fun start(primaryStage: Stage?) {
 
         val extent = Extent(.0, .0, 800.0, 600.0)
-        
-        val world = this.javaClass.getResourceAsStream("/world-110m.geojson")
+
+        world = this.javaClass.getResourceAsStream("/world-110m.geojson")
             .reader().readText().toGeoJsonObject()
 
         // OUTER GLOBE
@@ -33,11 +43,11 @@ class EarthApplication : Application() {
             translate = doubleArrayOf(400.0, 300.0)
             scale = 250.0
         }
-        val pathOuter = newPath().apply {
+        pathOuter = newPath().apply {
             stroke = colors.black
             fill = colors.white
         }
-        val geoPathOuter = geoPath(projectionOuter, pathOuter)
+        geoPathOuter = geoPath(projectionOuter, pathOuter)
         geoPathOuter.path(world)
 
         // INNER GLOBE
@@ -46,11 +56,11 @@ class EarthApplication : Application() {
             scale = 250.0
             clipAngle = Double.NaN          // remove angle clipping in order to see-through
         }
-        val pathVizJfxInner = newPath().apply {
+        pathVizJfxInner = newPath().apply {
             stroke = colors.grey
             fill = colors.grey
         }
-        val geoPathInner = geoPath(projectionInner, pathVizJfxInner)
+        geoPathInner = geoPath(projectionInner, pathVizJfxInner)
         geoPathInner.path(world)
 
         var initX = .0
@@ -63,8 +73,11 @@ class EarthApplication : Application() {
         primaryStage!!.scene = (Scene(root, extent.width, extent.height))
         primaryStage.show()
 
+        timer {now ->
+            loop(root, now)
+        }
 
-        root.setOnMousePressed { event ->
+        /*root.setOnMousePressed { event ->
             initX = event.x
             initY = event.y
             initRotate = geoPathInner.projection.rotate
@@ -85,8 +98,25 @@ class EarthApplication : Application() {
 
             geoPathOuter.path(world)
             geoPathInner.path(world)
-        }
+        }*/
 
+    }
+
+    fun loop(pane:Pane, now:Double) {
+        val rotate = geoPathInner.projection.rotate
+
+        rotate[0] += .5
+        rotate[1] = -10.0
+//        rotate[1] += .1
+
+        (pathVizJfxInner as PathVizJfx).jfxElement.elements.clear()
+        (pathOuter as PathVizJfx).jfxElement.elements.clear()
+
+        geoPathInner.projection.rotate = rotate
+        geoPathOuter.projection.rotate = rotate
+
+        geoPathOuter.path(world)
+        geoPathInner.path(world)
     }
 
 }
