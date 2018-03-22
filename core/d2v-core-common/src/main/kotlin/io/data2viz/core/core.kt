25d077012d8061fc3de45bd23b58443e6cda3088
pierre mariac <pmariac@gmail.com>
2018-03-22 20:07:41 +0100

    CORE : added minus operator for Point & Speed

--- core/d2v-core-common/src/main/kotlin/io/data2viz/core/core.kt
+++ core/d2v-core-common/src/main/kotlin/io/data2viz/core/core.kt
@@ -10,11 +10,13 @@ data class Point(val x: Double = 0.0, val y: Double = 0.0) {
     }
     operator fun plus(speed: Speed) = Point(x + speed.vx, y + speed.vy)
     operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+    operator fun minus(other: Point) = Point(x - other.x, y - other.y)
     operator fun div(value:Number) = Point(x/value.toDouble(), y/value.toDouble())
 }
 
 data class Speed(val vx: Double = 0.0, val vy: Double = 0.0) {
     operator fun plus(speed: Speed) = Speed(vx + speed.vx, vy + speed.vy)
+    operator fun minus(speed: Speed) = Speed(vx - speed.vx, vy - speed.vy)
 }
 
 expect fun random():Double
