27a0a8cd7bf08421eda48219b404237c4cb62437
gzoritchak <g.zoritchak@gmail.com>
2018-08-29 00:09:02 +0200

    refactor(ex-force-js): move to new model.

--- examples/ex-force/ex-force-js/src/main/kotlin/ExForceJs.kt
+++ examples/ex-force/ex-force-js/src/main/kotlin/ExForceJs.kt
@@ -1,11 +1,28 @@
 
-import io.data2viz.viz.ParentElement
-import io.data2viz.viz.selectOrCreateSvg
+import io.data2viz.timer.timer
+import io.data2viz.viz.JsCanvasRenderer
+import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLCanvasElement
+import kotlin.browser.document
 
 fun main(args: Array<String>) {
-    selectOrCreateSvg().apply { 
-        setAttribute("width", width.toString())
-        setAttribute("height", height.toString())
-        append((root as ParentElement).domElement)
+    val (canvas, context) = newCanvas(width.toInt(), height.toInt())
+    val viz = forcesViz
+    viz.renderer = JsCanvasRenderer(context)
+
+    timer {
+        viz.render()
     }
+
+}
+
+data class CanvasContext(val canvas: HTMLCanvasElement, val context2D: CanvasRenderingContext2D)
+
+private fun newCanvas(width: Int, height: Int): CanvasContext {
+    val canvas = document.createElement("canvas") as HTMLCanvasElement
+    val context = canvas.getContext("2d") as CanvasRenderingContext2D
+    context.canvas.width = width
+    context.canvas.height = height
+    document.querySelector("body")!!.appendChild(canvas)
+    return CanvasContext(canvas, context)
 }
