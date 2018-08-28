25d84e667c68012e8c293e5307346b0088ce1698
gzoritchak <g.zoritchak@gmail.com>
2018-08-28 19:41:27 +0200

    refactor(ex-sankey-js): move to new model.

--- examples/ex-sankey/ex-sankey-js/src/main/kotlin/ExSankeyJs.kt
+++ examples/ex-sankey/ex-sankey-js/src/main/kotlin/ExSankeyJs.kt
@@ -1,19 +1,30 @@
 package io.data2viz.examples.sankey
 
-import io.data2viz.viz.selectOrCreateSvg
-import io.data2viz.viz.viz
+import io.data2viz.viz.JsCanvasRenderer
+import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLCanvasElement
+import kotlin.browser.document
 
 
 @Suppress("unused")
 fun main(args: Array<String>) {
-    
-    val root = selectOrCreateSvg().apply {
-        setAttribute("width", "$vizWidth")
-        setAttribute("height", "$vizHeight")
-    }
 
-    root.viz {
-        sankeyViz()
-    }
+    val (canvas, context) = newCanvas(vizWidth.toInt(), vizHeight.toInt())
+    val viz = sankeyViz()
+    viz.renderer = JsCanvasRenderer(context)
+    viz.render()
+    println("ChordViz rendered")
 
+}
+
+
+data class CanvasContext(val canvas: HTMLCanvasElement, val context2D: CanvasRenderingContext2D)
+
+private fun newCanvas(width: Int, height: Int): CanvasContext {
+    val canvas = document.createElement("canvas") as HTMLCanvasElement
+    val context = canvas.getContext("2d") as CanvasRenderingContext2D
+    context.canvas.width = width
+    context.canvas.height = height
+    document.querySelector("body")!!.appendChild(canvas)
+    return CanvasContext(canvas, context)
 }
