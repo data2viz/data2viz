2ebc30f6acae3428062063ce348f0646cc13adfe
gzoritchak <g.zoritchak@gmail.com>
2018-08-30 11:03:44 +0200

    refactor(viz-common): Circle implement HasStroke.

--- viz/d2v-viz-common/src/main/kotlin/io/data2viz/viz/Circle.kt
+++ viz/d2v-viz-common/src/main/kotlin/io/data2viz/viz/Circle.kt
@@ -1,13 +1,17 @@
 package io.data2viz.viz
 
 import io.data2viz.color.Color
-import io.data2viz.color.colors
+import io.data2viz.color.ColorOrGradient
 
 
-class Circle : Node {
+class Circle : Node, HasStroke {
     var x: Double = 10.0
     var y: Double = 10.0
     var radius: Double = 10.0
+
     var fill: Color? = null
-    var stroke: Color? = null
+
+    override var stroke: ColorOrGradient? = null
+    override var strokeWidth: Double? = .0
+
 }
