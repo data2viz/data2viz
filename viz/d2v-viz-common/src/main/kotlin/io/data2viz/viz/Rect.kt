2f204c890a2e0825357041db5b0a330dd57b9b1e
gzoritchak <g.zoritchak@gmail.com>
2018-08-31 03:00:18 +0200

    feat(rect-common): set all value to .0 by default.

--- viz/d2v-viz-common/src/main/kotlin/io/data2viz/viz/Rect.kt
+++ viz/d2v-viz-common/src/main/kotlin/io/data2viz/viz/Rect.kt
@@ -5,10 +5,10 @@ import io.data2viz.color.colors
 
 
 class Rect : Node {
-    var x: Double = 10.0
-    var y: Double = 10.0
-    var width: Double = 10.0
-    var height: Double = 10.0
+    var x: Double = .0
+    var y: Double = .0
+    var width: Double = .0
+    var height: Double = .0
     var fill: Color? = null
     var stroke: Color? = null
     var strokeWidth: Double? = 1.0
