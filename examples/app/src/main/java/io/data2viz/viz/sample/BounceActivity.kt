cc5ba1774ae789e1ac7340b67a74c98bbf2997ab
gzoritchak <g.zoritchak@gmail.com>
2018-09-17 17:19:30 +0200

    fix(ex-bounce): stop and restart ok.

--- examples/app/src/main/java/io/data2viz/viz/sample/BounceActivity.kt
+++ examples/app/src/main/java/io/data2viz/viz/sample/BounceActivity.kt
@@ -1,48 +1,53 @@
 package io.data2viz.viz.sample
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import io.data2viz.color.colors
+import io.data2viz.logging.KotlinLogging
+import io.data2viz.timer.Timer
+import io.data2viz.timer.timer
+import io.data2viz.viz.Circle
 import io.data2viz.viz.VizView
 import io.data2viz.viz.toView
 import io.data2viz.viz.viz
 
+private val log = KotlinLogging.logger{}
 
 class BounceActivity : AppCompatActivity() {
 
     lateinit private var view: VizView
+    lateinit private var timer: Timer
+    lateinit var circle: Circle
+    val viz = viz {
+
+        width = 100.0
+        height = 100.0
+
+
+        circle = circle {
+            x = 0.0
+            y = 50.0
+            fill = colors.red
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         view = viz.toView(this)
         setContentView(view)
-        println("onCreate")
-        view.startAnimations()
+        log.debug { "onCreate"}
+        timer = timer {
+            if (circle.x > 100.0)
+                circle.x = .0
+            circle.x = circle.x + 1
+            view.invalidate()
+        }
     }
 
     override fun onStop() {
         super.onStop()
-        println("onStop")
-        view.stopAnimations()
-    }
-}
-
-
-val viz = viz {
-
-    width = 100.0
-    height = 100.0
-
-    val circle = circle {
-        x = 0.0
-        y = 50.0
-        fill = colors.red
-    }
-
-    onFrame { time ->
-        if (circle.x > 100.0)
-            circle.x = .0
-        circle.x = circle.x + 1
+        log.debug {"onStop"}
+        timer.stop()
     }
 }
