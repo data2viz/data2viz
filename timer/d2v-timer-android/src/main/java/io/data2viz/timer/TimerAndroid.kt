70ed556e1cb0aef034a5536e13aad7d058d20cd3
gzoritchak <g.zoritchak@gmail.com>
2018-09-17 17:15:48 +0200

    fix(timer): stoppable interval.

--- timer/d2v-timer-android/src/main/java/io/data2viz/timer/TimerAndroid.kt
+++ timer/d2v-timer-android/src/main/java/io/data2viz/timer/TimerAndroid.kt
@@ -2,47 +2,51 @@ package io.data2viz.timer
 
 import android.view.Choreographer
 import java.util.concurrent.TimeUnit
+import io.data2viz.logging.KotlinLogging
 
+private val log = KotlinLogging.logger{}
+
+val choreographer:Choreographer by lazy { Choreographer.getInstance() }
 
 internal actual fun setTimeout(handler: () -> Unit, timeout: Int): Any {
-    val callback: Choreographer.FrameCallback = Choreographer.FrameCallback {handler()}
-    val choreographer = Choreographer.getInstance()
+    val callback = Choreographer.FrameCallback {handler()}
     choreographer.postFrameCallbackDelayed(callback, timeout.toLong())
-    println("setTimeout:: $timeout, $choreographer $callback")
+    log.debug{"setTimeout:: $timeout ms, $callback"}
     return callback
 }
 
 internal actual fun clearTimeout(handle: Any) {
-    val choreographer = Choreographer.getInstance()
-    println("clearTimeout:: $choreographer $handle")
+    log.debug { "clearTimeout:: $handle"}
     choreographer.removeFrameCallback(handle as Choreographer.FrameCallback)
 }
 
-internal actual fun setInterval(handler: () -> Unit, interval: Int): Any{
-
-    val callback = Choreographer.FrameCallback {
-        handler()
-        setInterval(handler, interval)
-    }
-
-    Choreographer.getInstance()
-            .postFrameCallbackDelayed( callback, interval.toLong())
-
+internal actual fun setInterval(handler: () -> Unit, interval: Int): Any {
+    val callback = IntervalCallBack(interval, handler)
+    log.debug {"setInterval:: $interval $callback"}
+    intervalWithCB(callback, interval)
     return callback
 }
 
+class IntervalCallBack(var delay: Int, val block: () -> Unit): Choreographer.FrameCallback {
+    override fun doFrame(p0: Long) {
+        block()
+        intervalWithCB(this, delay)
+    }
+}
+
+private fun intervalWithCB(frameCallback: Choreographer.FrameCallback, interval: Int){
+    choreographer.postFrameCallbackDelayed(frameCallback, interval.toLong())
+
+}
+
 internal actual fun clearInterval(handle: Any) {
-    val choreographer = Choreographer.getInstance()
-    println("clearInterval:: $choreographer $handle")
+    log.debug { "clearInterval:: $handle" }
     choreographer.removeFrameCallback(handle as Choreographer.FrameCallback)
 }
 
 internal actual fun callInNextFrame(block: () -> Unit) {
-    val choreographer = Choreographer.getInstance()
-    println("callInNextFrame:: $choreographer")
     choreographer.postFrameCallback { block()}
 }
-
 
 internal actual fun delegateNow(): Double = TimeUnit.NANOSECONDS.toMillis(System.nanoTime()).toDouble()
 
