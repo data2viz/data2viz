15459a073f82c200ec5e2fa0b6c134e275e5f710
gzoritchak <g.zoritchak@gmail.com>
2018-09-21 20:51:11 +0200

    refactor(ex-geo-js): remove unused imports.

--- examples/ex-geo/ex-geo-js/src/main/kotlin/EarthJs.kt
+++ examples/ex-geo/ex-geo-js/src/main/kotlin/EarthJs.kt
@@ -1,25 +1,11 @@
 package io.data2viz.examples.geo
 
-import io.data2viz.color.colors
-import io.data2viz.geo.path.GeoPath
-import io.data2viz.geo.path.geoPath
-import io.data2viz.geo.projection.orthographic
-import io.data2viz.geojson.GeoJsonObject
 import io.data2viz.geojson.toGeoJsonObject
-import io.data2viz.timer.timer
-import io.data2viz.viz.JsCanvasRenderer
-import io.data2viz.viz.PathNode
-import io.data2viz.viz.Viz
 import io.data2viz.viz.bindRendererOn
 import kotlinx.coroutines.experimental.await
 import kotlinx.coroutines.experimental.promise
-import org.w3c.dom.CanvasRenderingContext2D
-import org.w3c.dom.Element
-import org.w3c.dom.HTMLCanvasElement
 import org.w3c.fetch.Request
-import kotlin.browser.document
 import kotlin.browser.window
-import kotlin.js.Date
 
 @Suppress("unused")
 
