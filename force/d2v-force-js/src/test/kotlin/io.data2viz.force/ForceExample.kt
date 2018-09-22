f5678312ea5db4f3e389cc8b049b3b9f46b9024e
gzoritchak <g.zoritchak@gmail.com>
2018-09-22 17:20:34 +0200

    feat(force-js): new example with links.

--- force/d2v-force-js/src/test/kotlin/io.data2viz.force/ForceExample.kt
+++ force/d2v-force-js/src/test/kotlin/io.data2viz.force/ForceExample.kt
@@ -1,11 +1,9 @@
 package io.data2viz.force
 
 import io.data2viz.color.colors
-import io.data2viz.geom.Point
 import io.data2viz.math.random
-import io.data2viz.viz.Circle
-import io.data2viz.viz.bindRendererOn
-import io.data2viz.viz.viz
+import io.data2viz.viz.*
+import kotlin.js.Math
 import kotlin.math.roundToInt
 import kotlin.math.floor
 import kotlin.math.sqrt
@@ -15,45 +13,62 @@ const val nodeCount = 1000
 const val width = 1000.0
 const val height = 1000.0
 
-val nodes = (0 until nodeCount).map { ForceNode(it, random() * width, random() * height) }
-val links = (0 until nodeCount-1).map { Link(nodes[floor(sqrt(it.toDouble())).roundToInt()], nodes[it +1]) }
+val fnodes = (0 until nodeCount).map { ForceNode(it, random() * width - width/2, random() * height - height/2) }
+val links = (0 until nodeCount - 1).map { Link(fnodes[floor(sqrt(it.toDouble())).roundToInt()], fnodes[it + 1]) }
+
+val sim = forceSimulation {
+
+    nodes = fnodes
+
+    addForce("charge", forceNBody())
+    addForce("link", forceLink(links))
+    addForce("x", forceX())
+    addForce("y", forceY())
+    on(SimulationEvent.TICK, "tickEvent", ::refresh)
+    on(SimulationEvent.END, "endEvent") { println("SIMULATION ENDS") }
+}
+
+val renderGroup: Group = Group().apply {
+    transform {
+        translate( width / 2, height / 2)
+    }
+}
+
+val viz = viz {
+    width = 1000.0
+    height = 1000.0
+    add(renderGroup)
+
+    onFrame {
+        refresh(sim)
+    }
+}
+
+fun refresh(sim: ForceSimulation) {
+    renderGroup.apply {
+        children.clear()
+        path {
+            links.forEach { link ->
+                moveTo(link.source.x, link.source.y)
+                lineTo(link.target.x, link.target.y)
+            }
+            strokeWidth = 1.0
+            stroke = colors.grey
+        }
+        path {
+            sim.nodes.forEach { d ->
+                moveTo(d.x + 3, d.y)
+                arc(d.x, d.y, 3.0, 0.0, 2 * Math.PI);
+            }
+            fill = colors.black
+            stroke = colors.white
+            strokeWidth = 1.0
+        }
+    }
+
+}
+
 
 fun main(args: Array<String>) {
-
-    val viz = viz {
-        width = 1000.0
-        height = 1000.0
-
-        rect {
-            x = 100.0
-            y = 100.0
-            width = 100.0
-            height = 100.0
-            fill = colors.red
-        }
-    }
-
-
-    fun refresh(sim: ForceSimulation) {
-        viz.activeLayer.children.forEachIndexed { index, node ->
-            val forceNode = sim.nodes[index]
-            val circle = node as Circle
-            circle.x = forceNode.x
-            circle.y = forceNode.y
-        }
-    }
-
-
-    forceSimulation {
-        addForce("charge", forceNBody {  })
-//        addForce("link", forceLink { from, to ->  })
-        addForce("x", forceX {  })
-        addForce("y", forceY {  })
-        on(SimulationEvent.TICK, "tickEvent", ::refresh)
-        on(SimulationEvent.END, "endEvent") { println("SIMULATION ENDS") }
-    }
-
     viz.bindRendererOn("viz")
-    println("yo")
-
 }
