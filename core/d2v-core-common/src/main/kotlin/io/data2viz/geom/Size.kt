d40600e6277eb7599b86518f04198ff141aadbf8
gzoritchak <g.zoritchak@gmail.com>
2018-10-24 01:39:49 +0200

    documentation(core): Size documentation.

--- core/d2v-core-common/src/main/kotlin/io/data2viz/geom/Size.kt
+++ core/d2v-core-common/src/main/kotlin/io/data2viz/geom/Size.kt
@@ -1,8 +1,15 @@
 package io.data2viz.geom
 
+/**
+ * Size represents a rectangle dimension. The second parameter of the constructor (height)
+ * is option and takes by default the same value as the width.
+ */
+data class Size(val width: Double, val height: Double = width)
 
-data class Size(val width: Double, val height: Double)
-
+/**
+ * Indicates a class that has a width and a height. Adds an size property to
+ * set and get width and height in a single line.
+ */
 interface HasSize {
 
     var width: Double
