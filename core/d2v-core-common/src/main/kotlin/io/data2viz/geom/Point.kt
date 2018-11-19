3d0d35a4201634ca40fecf89fead2997a985a7ed
gzoritchak <g.zoritchak@gmail.com>
2018-11-19 12:48:56 +0100

    refactor(core): Point.negate() -> Point.unaryMinus()

--- core/d2v-core-common/src/main/kotlin/io/data2viz/geom/Point.kt
+++ core/d2v-core-common/src/main/kotlin/io/data2viz/geom/Point.kt
@@ -20,5 +20,6 @@ data class Point(val x: Double = 0.0, val y: Double = 0.0) {
     operator fun minus(other: Point) = Point(x - other.x, y - other.y)
     operator fun div(value:Number) = Point(x / value.toDouble(), y / value.toDouble())
     operator fun times(value:Number) = Point(x * value.toDouble(), y * value.toDouble())
-    fun negate(): Point = Point(-x, -y)
+
+    operator fun unaryMinus(): Point = Point(-x, -y)
 }
