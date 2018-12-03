6d615d44fade02027fef199adf5f65bc774e4d90
pierre mariac <pmariac@gmail.com>
2018-12-03 12:34:21 +0100

    feat (FORCE) change forceEX JFX example to stress-test forceLink

--- force/d2v-force-jfx/src/test/kotlin/io/data2viz/force/ForceExJFX.kt
+++ force/d2v-force-jfx/src/test/kotlin/io/data2viz/force/ForceExJFX.kt
@@ -2,38 +2,46 @@ import io.data2viz.color.*
 import io.data2viz.geom.*
 import io.data2viz.viz.*
 import io.data2viz.force.*
+import io.data2viz.timer.now
 import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
+import java.util.*
+import kotlin.math.sqrt
 
 data class Item(val name:String, val index:Int, val depth: Int, val parents:List<Int> = listOf())
 
-val items = listOf(
-    Item("ROOT", 0, 0),
-    Item("A", 1, 1, listOf(0)),
-    Item("A1", 2, 2, listOf(1)),
-    Item("A2", 3, 2, listOf(1)),
-    Item("A3", 4, 2, listOf(1)),
-    Item("A4", 5, 2, listOf(1, 8)),
-    Item("B", 6, 1, listOf(0)),
-    Item("B1", 7, 2, listOf(6)),
-    Item("B2", 8, 2, listOf(6)),
-    Item("B3", 9, 2, listOf(6)),
-    Item("C", 10, 1, listOf(0)),
-    Item("C1", 11, 2, listOf(10)),
-    Item("A31", 12, 3, listOf(4, 13, 14)),
-    Item("A32", 13, 3, listOf(4)),
-    Item("A33", 14, 3, listOf(4))
-)
+//val items = listOf(
+//    Item("ROOT", 0, 0),
+//    Item("A", 1, 1, listOf(0)),
+//    Item("A1", 2, 2, listOf(1)),
+//    Item("A2", 3, 2, listOf(1)),
+//    Item("A3", 4, 2, listOf(1)),
+//    Item("A4", 5, 2, listOf(1, 8)),
+//    Item("B", 6, 1, listOf(0)),
+//    Item("B1", 7, 2, listOf(6)),
+//    Item("B2", 8, 2, listOf(6)),
+//    Item("B3", 9, 2, listOf(6)),
+//    Item("C", 10, 1, listOf(0)),
+//    Item("C1", 11, 2, listOf(10)),
+//    Item("A31", 12, 3, listOf(4, 13, 14)),
+//    Item("A32", 13, 3, listOf(4)),
+//    Item("A33", 14, 3, listOf(4))
+//)
 
-val vizSize = 600.0
+val items = (0..2500).map { Item("$it", it, it, listOf(sqrt(it.toDouble()).toInt()))}
+
+val vizSize = 1000.0
+val timer = now()
 
 class ForceExJFX: Application() {
     val canvas = Canvas(vizSize, vizSize)
     val viz = graph()
     val renderer = JFxVizRenderer(canvas, viz)
+
+
 
     init {
         viz.renderer = renderer
@@ -81,20 +89,24 @@ fun graph(): Viz {
         }
 
         val f = forceLink {
-            linkGet = { domain.parents.map { Link(this, nodes[it]) } }
-            iterations = 10
+            linkGet = { domain.parents.map { Link(this, nodes[it], 20.0, 1.0) } }
+            //iterations = 10
         }
         links = f.links
 
         forceCenter {
             center = Point(vizSize / 2, vizSize / 2)
         }
-        forceCollision {
-            radiusGet = { (5 - domain.depth) * 15.0 }
-            iterations = 10
-        }
+//        forceCollision {
+//            radiusGet = { (5 - domain.depth) * 5.0 }
+//            iterations = 1
+//        }
+        forceNBody()
+
+        on(SimulationEvent.END, "end", {
+            println("${now() - timer}ms")
+        })
     }
-    links.forEach { println(it) }
 
     val myViz = viz {
         size = size(vizSize, vizSize)
@@ -107,20 +119,20 @@ fun graph(): Viz {
                 stroke = Colors.Web.grey
             }
         }
-        simulation.nodes.forEach {
+        /*simulation.nodes.forEach {
             particles += text {
                 fill = Colors.Web.black
                 textContent = items[it.index].name
                 textAlign = textAlign(TextHAlign.MIDDLE, TextVAlign.MIDDLE)
                 fontWeight = FontWeight.BOLD
             }
-        }
+        }*/
 
         animation {
-            simulation.nodes.forEach { node ->
+            /*simulation.nodes.forEach { node ->
                 particles[node.index].x = node.x
                 particles[node.index].y = node.y
-            }
+            }*/
             links.forEachIndexed { index, _ ->
                 particleLinks[index].x1 = links[index].source.x
                 particleLinks[index].x2 = links[index].target.x
