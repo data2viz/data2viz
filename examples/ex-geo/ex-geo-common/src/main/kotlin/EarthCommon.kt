81e29f9acb6f6fa6c0e32b45c1f18660c1243ed8
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2019-05-07 02:59:01 +0300

    add FPS meter to EarthCommon Geo Sample Common code

--- examples/ex-geo/ex-geo-common/src/main/kotlin/EarthCommon.kt
+++ examples/ex-geo/ex-geo-common/src/main/kotlin/EarthCommon.kt
@@ -8,10 +8,18 @@ import io.data2viz.viz.PathNode
 import io.data2viz.viz.Viz
 import io.data2viz.viz.viz
 import kotlin.math.min
+import kotlin.math.roundToInt
+
 
 fun geoViz(world: GeoJsonObject): Viz = viz {
     width = 960.0
     height = 700.0
+
+    val fps = text {
+        x = 40.0
+        y = 40.0
+        fill = Colors.Web.red
+    }
 
     // OUTER GLOBE
     val projectionOuter = orthographic {
@@ -30,7 +38,15 @@ fun geoViz(world: GeoJsonObject): Viz = viz {
     geoPathOuter.path(world)
     add(pathOuter)
 
-    animation {
+    animation { now: Double ->
+
+        FPS.eventuallyUpdate(now)
+
+        if (FPS.value >= 0) {
+            fps.textContent = "${FPS.value.roundToInt()} FPS"
+        }
+
+
         val rotate = geoPathOuter.projection.rotate
         rotate[0] += .5
         rotate[1] = -10.0
@@ -49,5 +65,28 @@ fun geoViz(world: GeoJsonObject): Viz = viz {
             translate = doubleArrayOf(width / 2.0, height / 2.0)
             scale = min(width, height) / 2.2
         }, pathOuter)
+    }
+
+
+}
+
+object FPS {
+    val averageCount = 10
+    var value = .0
+    var count = 0
+    var lastStart = Double.NaN
+
+    /**
+     * current: current time in ms.
+     */
+    fun eventuallyUpdate(current: Double) {
+        if (lastStart == Double.NaN)
+            lastStart = current
+        if (count++ == averageCount) {
+            val totalTime = current - lastStart
+            value = 1.0e3 * averageCount / totalTime
+            lastStart = current
+            count = 0
+        }
     }
 }
