fed5be7d0bbf940ccb5ae4c89db4f01afdef04b5
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2019-05-11 01:10:06 +0300

    geo: projection: Gnomonic

---
+++ geo/d2v-geo-common/src/main/kotlin/io/data2viz/geo/projection/Gnomonic.kt
@@ -0,0 +1,40 @@
+package io.data2viz.geo.projection
+
+import kotlin.math.atan
+import kotlin.math.cos
+import kotlin.math.sin
+
+
+fun gnomonicProjection() = gnomonicProjection {
+
+}
+
+fun gnomonicProjection(init: Projection.() -> Unit) = projection(IdentityProjection()) {
+    clipAngle = 60.0
+
+    scale = 144.049
+    init()
+}
+
+class GnomonicProjector : ProjectableInvertable {
+    override fun project(x: Double, y: Double): DoubleArray {
+        val cy = cos(y)
+        val k = cos(x) * cy;
+        return doubleArrayOf(cy * sin(x) / k, sin(y) / k)
+    }
+
+    // TODO: Refactor
+    override fun invert(x: Double, y: Double): DoubleArray = azimuthalInvert(::atan)(x, y)
+
+    override fun projectLambda(x: Double, y: Double): Double {
+        val cy = cos(y)
+        val k = cos(x) * cy;
+        return cy * sin(x) / k
+    }
+
+    override fun projectPhi(x: Double, y: Double): Double {
+        val cy = cos(y)
+        val k = cos(x) * cy;
+        return sin(y) / k
+    }
+}
