6bbec75d347bf2ce4063363b2c850d65914a7ea6
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2019-05-25 01:15:30 +0300

    geo: pass albers USA test

--- geo/d2v-geo-common/src/main/kotlin/io/data2viz/geo/projection/AlbersUSA.kt
+++ geo/d2v-geo-common/src/main/kotlin/io/data2viz/geo/projection/AlbersUSA.kt
@@ -17,6 +17,7 @@ fun albersUSAProjection(init: AlbersUSAProjection.() -> Unit) = AlbersUSAProject
 
 
 /**
+ * TODO: return project Double.NAN when projeciton not possible, for example projecting area outside US
  * This is a U.S.-centric composite projection of three d3.geoConicEqualArea projections: d3.geoAlbers is used for the lower forty-eight states, and separate conic equal-area projections are used for Alaska and Hawaii. Note that the scale for Alaska is diminished: it is projected at 0.35× its true relative area. This diagram by Philippe Rivière illustrates how this projection uses two rectangular insets for Alaska and Hawaii:
  */
 //// A composite projection for the United States, configured by default for
