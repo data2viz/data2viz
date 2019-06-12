0c323ba482e9d7e134a72d3a49cfff60809ec395
gzoritchak <g.zoritchak@gmail.com>
2019-06-12 23:48:59 +0200

    geo: minor refactoring, extract TranslateAndScaleProjector

--- geo/d2v-geo-common/src/main/kotlin/io/data2viz/geo/projection/common/ProjectorProjection.kt
+++ geo/d2v-geo-common/src/main/kotlin/io/data2viz/geo/projection/common/ProjectorProjection.kt
@@ -216,36 +216,5 @@ open class ProjectorProjection(val projector: Projector) : Projection {
         translateAndScaleProjector.recenterDy = _recenterDy
     }
 
-
-}
-
-/**
- * Scale & translate projector based on values from [projector]
- *
- */
-class TranslateAndScaleProjector(
-    val projector: Projector,
-    var scale: Double,
-    var recenterDx: Double,
-    var recenterDy: Double
-) : Projector {
-
-    override fun project(lambda: Double, phi: Double): DoubleArray {
-        val projected = projector.project(lambda, phi)
-        projected[0] = recenterDx + projected[0] * scale
-        projected[1] = recenterDy - projected[1] * scale
-        return projected
-    }
-
-
-    // TODO: need re-check. invert not exist in d3 implementation
-    override fun invert(x: Double, y: Double): DoubleArray {
-
-        return projector.invert(
-            (x - recenterDx) / scale,
-            -(y - recenterDy) / scale
-        )
-    }
-
 }
 
---
+++ geo/d2v-geo-common/src/main/kotlin/io/data2viz/geo/projection/common/TranslateAndScaleProjector.kt
@@ -0,0 +1,31 @@
+package io.data2viz.geo.projection.common
+
+/**
+ * Scale & translate projector based on values from [projector]
+ *
+ */
+class TranslateAndScaleProjector(
+    val projector: Projector,
+    var scale: Double,
+    var recenterDx: Double,
+    var recenterDy: Double
+) : Projector {
+
+    override fun project(lambda: Double, phi: Double): DoubleArray {
+        val projected = projector.project(lambda, phi)
+        projected[0] = recenterDx + projected[0] * scale
+        projected[1] = recenterDy - projected[1] * scale
+        return projected
+    }
+
+
+    // TODO: need re-check. invert not exist in d3 implementation
+    override fun invert(x: Double, y: Double): DoubleArray {
+
+        return projector.invert(
+            (x - recenterDx) / scale,
+            -(y - recenterDy) / scale
+        )
+    }
+
+}
