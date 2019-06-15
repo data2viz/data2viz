a9df60a4a6db6c08a07db644cf284e2139776420
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2019-06-15 18:48:45 +0300

    geo: fix Angle Pre Clip for over 360 degrees values

--- geo/d2v-geo-common/src/main/kotlin/io/data2viz/geo/geometry/clip/AnglePreClip.kt
+++ geo/d2v-geo-common/src/main/kotlin/io/data2viz/geo/geometry/clip/AnglePreClip.kt
@@ -3,10 +3,12 @@ package io.data2viz.geo.geometry.clip
 import io.data2viz.geo.projection.common.Projection
 import io.data2viz.geo.stream.Stream
 import io.data2viz.math.Angle
+import io.data2viz.math.toRadians
 
 private class AnglePreClip(val angle: Angle) : ClipStreamBuilder {
 
-    val clipCircle = CirclePreClip(angle.rad)
+    val transformedAngleInDegrees = (angle.deg % 360)
+    val clipCircle = CirclePreClip(transformedAngleInDegrees.toRadians())
 
     override fun bindTo(downstream: Stream): Stream {
         return clipCircle.bindTo(downstream)
