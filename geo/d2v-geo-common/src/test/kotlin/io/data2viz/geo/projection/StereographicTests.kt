ac598756438c997f6ab5fb90ddc1f9c5e0b11726
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2019-06-15 20:08:08 +0300

    geo: add stereographic tests

--- geo/d2v-geo-common/src/test/kotlin/io/data2viz/geo/projection/StereographicTests.kt
+++ geo/d2v-geo-common/src/test/kotlin/io/data2viz/geo/projection/StereographicTests.kt
@@ -1,17 +1,22 @@
 package io.data2viz.geo.projection
 
 
-//var tape = require("tape"),
-//d3 = require("../../");
-//
-//require("./projectionEqual");
-//
-//tape("stereographic(point) returns the expected result", function(test) {
-//    var stereographic = d3.geoStereographic().translate([0, 0]).scale(1);
-//    test.projectionEqual(stereographic, [  0,   0], [ 0,  0]);
-//    test.projectionEqual(stereographic, [-90,   0], [-1,  0]);
-//    test.projectionEqual(stereographic, [ 90,   0], [ 1,  0]);
-//    test.projectionEqual(stereographic, [  0, -90], [ 0,  1]);
-//    test.projectionEqual(stereographic, [  0,  90], [ 0, -1]);
-//    test.end();
-//});
+import kotlin.test.Test
+
+class StereographicTests {
+
+    @Test
+    fun stereographic_returns_expected_result() {
+        val projection = stereographicProjection {
+            translate(0.0, 0.0)
+            scale = 1.0
+        }
+
+        checkProjectAndInvert(projection, 0.0, 0.0, 0.0, 0.0)
+        checkProjectAndInvert(projection, -90.0, 0.0, -1.0, 0.0)
+        checkProjectAndInvert(projection, 90.0, 0.0, 1.0, 0.0)
+        checkProjectAndInvert(projection, 0.0, -90.0, 0.0, 1.0)
+        checkProjectAndInvert(projection, 0.0, 90.0, 0.0, -1.0)
+
+    }
+}
