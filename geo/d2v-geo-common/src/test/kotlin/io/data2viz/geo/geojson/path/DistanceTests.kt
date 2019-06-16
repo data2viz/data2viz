facac84fcf46adc57eecc65bc439c83e6e27b7a6
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2019-06-16 23:51:51 +0300

    geo: add geoDistance tests

--- geo/d2v-geo-common/src/test/kotlin/io/data2viz/geo/geojson/path/DistanceTests.kt
+++ geo/d2v-geo-common/src/test/kotlin/io/data2viz/geo/geojson/path/DistanceTests.kt
@@ -1,18 +1,32 @@
 package io.data2viz.geo.geojson.path
 
+import io.data2viz.geo.projection.inDelta
+import io.data2viz.geo.projection.pt
+import io.data2viz.geojson.Point
+import io.data2viz.test.TestBase
+import kotlin.test.Test
 
-//var tape = require("tape"),
-//d3 = require("../");
-//
-//require("./inDelta");
-//
-//tape("geoDistance(a, b) computes the great-arc distance in radians between the two points a and b", function(test) {
-//    test.equal(d3.geoDistance([0, 0], [0, 0]), 0);
-//    test.inDelta(d3.geoDistance([118 + 24 / 60, 33 + 57 / 60], [73 + 47 / 60, 40 + 38 / 60]), 3973 / 6371, 0.5);
-//    test.end();
-//});
-//
-//tape("geoDistance(a, b) correctly computes small distances", function(test) {
-//    test.assert(d3.geoDistance([0, 0], [0, 1e-12]) > 0);
-//    test.end();
-//});
+class DistanceTests : TestBase() {
+    @Test
+    fun geoDistance_a_b_computes_the_great_arc_distance_in_radians_between_the_two_points_a_and_b() {
+
+        geoDistance(arrayOf(0.0, 0.0), arrayOf(0.0, 0.0)) shouldBe 0.0
+
+
+        inDelta(geoDistance(
+            arrayOf(118.0 + 24.0 / 60.0, 33.0 + 57.0 / 60.0),
+            arrayOf(73.0 + 47.0 / 60.0, 40.0 + 38.0 / 60.0)
+        ), 3973.0 / 6371.0, 0.5)
+
+    }
+
+    @Test
+    fun geoDistance_a_b_correctly_computes_small_distances() {
+
+        val geoDistance = geoDistance(arrayOf(0.0, 0.0), arrayOf(0.0, 1.0 / 1000000000000.0))
+        if(geoDistance <= 0.0) {
+            error("geoDistance $geoDistance should more than 0.0")
+        }
+
+    }
+}
