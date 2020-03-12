bd552ab4a9152ac65940ddf46e75cda6b1669cfa
pierre mariac <pmariac@gmail.com>
2020-03-12 13:33:46 +0100

    Improve the Axis Element, give acces to axis stroke, color... Also remove the offset of axis lines for better alignment.

--- axis/src/commonMain/kotlin/io/data2viz/axis/AxisCommon.kt
+++ axis/src/commonMain/kotlin/io/data2viz/axis/AxisCommon.kt
@@ -17,103 +17,127 @@
 
 package io.data2viz.axis
 
+import io.data2viz.color.ColorOrGradient
 import io.data2viz.color.Colors
-import io.data2viz.scale.*
+import io.data2viz.scale.BandedScale
+import io.data2viz.scale.FirstLastRange
+import io.data2viz.scale.Scale
+import io.data2viz.scale.Tickable
 import io.data2viz.viz.*
 import kotlin.math.round
 
 
 /**
- * Create an Axis 
+ * Create an Axis
  */
-fun <D> GroupNode.axis(orient: Orient, scale: FirstLastRange<D,Double>, init:AxisElement<D>.() -> Unit = {}):AxisElement<D> =
+fun <D> GroupNode.axis(
+    orient: Orient,
+    scale: FirstLastRange<D, Double>,
+    init: AxisElement<D>.() -> Unit = {}
+): AxisElement<D> =
 
-        AxisElement(orient, scale).apply {
-            init(this)
-            build(this@axis)
-        }
+    AxisElement(orient, scale).apply {
+        init(this)
+        build(this@axis)
+    }
 
+class AxisElement<D>(val orient: Orient, val scale: FirstLastRange<D, Double>) {
 
-class AxisElement<D>(val orient: Orient, val scale: FirstLastRange<D,Double>)  {
-
-    var tickValues:List<D> = listOf()
+    var tickValues: List<D> = listOf()
     var tickSizeInner = 6.0
     var tickSizeOuter = 6.0
     var tickPadding = 3.0
-    var tickFormat = {n:D -> n.toString()}
+    var stroke: ColorOrGradient = Colors.Web.black
+    var strokeWidth: Double = 1.0
+    var fontSize: Double = 12.0
+    var fontColor: ColorOrGradient = Colors.Web.black
+    var fontFamily: FontFamily = FontFamily.SANS_SERIF
+    var fontWeight: FontWeight = FontWeight.NORMAL
+    var fontStyle: FontPosture = FontPosture.NORMAL
 
-    val k = if (orient == Orient.TOP || orient == Orient.LEFT) -1 else 1
+    var tickFormat = { n: D -> n.toString() }
 
-    fun center(scale: BandedScale<D>): (D) -> Double {
+    private val k = if (orient == Orient.TOP || orient == Orient.LEFT) -1 else 1
+
+    private fun center(scale: BandedScale<D>): (D) -> Double {
         var offset = (scale.bandwidth - 1).coerceAtLeast(0.0) / 2 // Adjust for 0.5px offset.
         if (scale.round) offset = round(offset)
-        return { d:D -> +scale(d) + offset }
+        return { d: D -> +scale(d) + offset }
     }
 
-    fun number(scale: Scale<D,Double>) : (D) -> Double  = { d -> scale(d) }
+    private fun number(scale: Scale<D, Double>): (D) -> Double = { scale(it) }
 
     fun build(content: GroupNode) {
-        
+
         val values: List<D> = if (tickValues.isEmpty() && scale is Tickable<*>) scale.ticks() as List<D> else tickValues
         val spacing = tickSizeInner.coerceAtLeast(0.0) + tickPadding
-        val range0 = scale.start()
-        val range1 = scale.end()
+        val start = scale.start()
+        val end = scale.end()
         val position = if (scale is BandedScale) center(scale) else number(scale)
 
-        with(content){
-            path {
-                stroke = Colors.Web.black
-                fill = null
-                strokeWidth = 1.0
+        with(content) {
 
-                if(orient.isVertical()) {
-                    moveTo(tickSizeOuter * k, range0)
-                    lineTo(- 0.5 * k, range0)
-                    lineTo(- 0.5 * k, range1)
-                    lineTo(tickSizeOuter * k, range1)
+            // the main axis line
+            path {
+                stroke = this@AxisElement.stroke
+                fill = null
+                strokeWidth = this@AxisElement.strokeWidth
+
+                if (orient.isVertical()) {
+                    moveTo(tickSizeOuter * k, start)
+                    lineTo(.0, start)
+                    lineTo(.0, end)
+                    lineTo(tickSizeOuter * k, end)
                 } else {
-                    moveTo(range0, tickSizeOuter * k)
-                    lineTo(range0, - 0.5 * k)
-                    lineTo(range1, - 0.5 * k)
-                    lineTo(range1, tickSizeOuter * k)
+                    moveTo(start, tickSizeOuter * k)
+                    lineTo(start, .0)
+                    lineTo(end, .0)
+                    lineTo(end, tickSizeOuter * k)
                 }
             }
-            
+
+            // each tick and associated label
             values.forEach {
-                group { 
-                    transform { 
+                group {
+                    transform {
                         if (orient.isHorizontal()) translate(x = position(it)) else translate(y = position(it))
                     }
                     if (orient.isHorizontal())
                         line {
                             y2 = k * tickSizeInner
-                            stroke = Colors.Web.black
+                            stroke = this@AxisElement.stroke
+                            strokeWidth = this@AxisElement.strokeWidth
                         }
                     else
                         line {
                             x2 = k * tickSizeInner
-                            stroke = Colors.Web.black
+                            stroke = this@AxisElement.stroke
+                            strokeWidth = this@AxisElement.strokeWidth
                         }
                     text {
+                        textColor = fontColor
+                        fontWeight = this@AxisElement.fontWeight
+                        fontSize = this@AxisElement.fontSize
+                        fontFamily = this@AxisElement.fontFamily
+                        fontStyle = this@AxisElement.fontStyle
                         hAlign = when (orient) {
                             Orient.LEFT -> TextHAlign.RIGHT
                             Orient.RIGHT -> TextHAlign.LEFT
                             else -> TextHAlign.MIDDLE
                         }
-                        
-                        vAlign = when (orient){
+
+                        vAlign = when (orient) {
                             Orient.TOP -> TextVAlign.BASELINE
                             Orient.BOTTOM -> TextVAlign.HANGING
                             else -> TextVAlign.MIDDLE
                         }
-                        fill = Colors.Web.black
-                        if(orient.isHorizontal()) 
+                        if (orient.isHorizontal())
                             y = spacing * k
                         else
                             x = spacing * k
                         textContent = tickFormat(it)
                     }
-                    
+
                 }
             }
         }
@@ -123,7 +147,7 @@ class AxisElement<D>(val orient: Orient, val scale: FirstLastRange<D,Double>)  {
 
 enum class Orient {
     TOP, BOTTOM, LEFT, RIGHT;
-    
+
     fun isVertical() = (this == LEFT || this == RIGHT)
     fun isHorizontal() = (this == TOP || this == BOTTOM)
 }
