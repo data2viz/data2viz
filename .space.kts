6c22d4e85b074a0357b204bcfc1eb50d868a9795
gzoritchak <g.zoritchak@gmail.com>
2020-10-10 18:38:09 +0200

    chore: space-automation use gradle container

--- .space.kts
+++ .space.kts
@@ -2,14 +2,10 @@
 
 
 job("Build and run core") {
-    container("adoptopenjdk/openjdk8") {
-        resources {
-            memory = 768
-        }
-
-        kotlinScript { api ->
-            // here goes complex logic
-            api.gradlew("core:build")
+    container("gradle") {
+        // run 'gradle build'
+        shellScript {
+            content = "gradle build"
         }
     }
 }
