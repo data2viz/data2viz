4f38c34ed53eb173ee2633c220f8185db34f0774
gzoritchak <g.zoritchak@gmail.com>
2020-10-11 01:52:12 +0200

    chore: space-automation :core:build with only js and no test

--- .space.kts
+++ .space.kts
@@ -2,7 +2,7 @@
 job("Build and run core") {
     container("gradle:6.6-jdk8") {
         shellScript {
-            content = "gradle :core:build -x test -Pinclude_android=false -Pinclude_js=true -Pinclude_jfx=false --stacktrace"
+            content = "gradle :core:build -x jsTest -x jsBrowserTest -x jvmTest  -Pinclude_android=false -Pinclude_js=true -Pinclude_jfx=false --stacktrace"
         }
     }
 }
