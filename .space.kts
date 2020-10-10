430af8c20162fa3088857048cc72b18e97f514f1
gzoritchak <g.zoritchak@gmail.com>
2020-10-11 01:41:25 +0200

    chore: space-automation :core:build -x test with shell script

--- .space.kts
+++ .space.kts
@@ -1,8 +1,8 @@
 
 job("Build and run core") {
     container("gradle:6.6-jdk8") {
-        kotlinScript { api ->
-            api.gradle(":core:build", "-x", "test")
+        shellScript {
+            content = "gradle :core:build -x test"
         }
     }
 }
