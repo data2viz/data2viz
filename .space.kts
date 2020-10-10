fb06cee075106f572ff12cbc20af0c6f1a68e595
gzoritchak <g.zoritchak@gmail.com>
2020-10-10 16:29:40 +0200

    build core on space

--- .space.kts
+++ .space.kts
@@ -1,5 +1,15 @@
 
 
-job("Hello World!") {
-    container("hello-world")
+
+job("Build and run core") {
+    container("adoptopenjdk/openjdk8") {
+        resources {
+            memory = 768
+        }
+
+        kotlinScript { api ->
+            // here goes complex logic
+            api.gradlew("core:build")
+        }
+    }
 }
