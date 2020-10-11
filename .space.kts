e56216f24cbdb58bc9048ca222887f4c799960fc
gzoritchak <g.zoritchak@gmail.com>
2020-10-11 02:46:52 +0200

    chore: space-automation core module with jfxTest

--- .space.kts
+++ .space.kts
@@ -2,7 +2,7 @@
 job("Build and test JFx") {
     container("gradle:6.6-jdk8") {
         shellScript {
-            content = "gradle build -x jsBrowserTest -x jsTest -Pinclude_android=false -Pinclude_js=false -Pinclude_jfx=true --stacktrace"
+            content = "gradle build -Pinclude_android=false -Pinclude_js=false -Pinclude_jfx=true --stacktrace"
         }
     }
 }
