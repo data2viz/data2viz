62d54759068777c356a82285a6447964d3ae8a75
Jolanrensen <j.j.r.rensen@student.tue.nl>
2021-10-17 18:01:17 +0200

    started converting tests to universal format. Still issues with encoding doubles to strings it seems.

--- viz/src/commonTest/kotlin/io/data2viz.viz/TestVizToSVG.kt
+++ viz/src/commonTest/kotlin/io/data2viz.viz/TestVizToSVG.kt
@@ -64,10 +64,10 @@ class TestVizToSVG : TestBase() {
         }
 
         simpleSvg shouldBe """
-            <?xml version="1.0"?>
-            <svg width="200" height="200" version="1.0" xmlns="http://www.w3.org/2000/svg">
-            <rect x="10" y="10" width="30" height="30" style="stroke:red;fill:transparent;stroke-width:5" transform="rotate(45 25 25)"/>
-            <rect x="60" y="10" rx="10" ry="5" width="30" height="30" style="stroke:black;fill:transparent;stroke-width:5"/>
+            <?xml version="${1.0}"?>
+            <svg width="${200}" height="${200}" version="${1.0}" xmlns="http://www.w3.org/2000/svg">
+            <rect x="${10}" y="${10}" width="${30}" height="${30}" style="stroke:red;fill:transparent;stroke-width:${5}" transform="rotate(${45} ${25} ${25})"/>
+            <rect x="${60}" y="${10}" rx="${10}" ry="${5}" width="${30}" height="${30}" style="stroke:black;fill:transparent;stroke-width:${5}"/>
             </svg>
 
             """.trimIndent()
@@ -102,19 +102,20 @@ class TestVizToSVG : TestBase() {
         }.toSVG()
 
         val target = """
-            <?xml version="1.0"?>
-            <svg xmlns="http://www.w3.org/2000/svg" width="100.0" height="100.0">
+            <?xml version="${1.0}"?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="${100.0}" height="${100.0}">
             <g>
-            <text x="10.0" y="5.845703125" font-size="12.0" font-family="sans-serif" font-weight="NORMAL" font-style="NORMAL" style="stroke-width:1.0">
+            <text x="${10.0}" y="${5.845703125}" font-size="${12.0}" font-family="sans-serif" font-weight="NORMAL" font-style="NORMAL" style="stroke-width:${1.0}">
             O</text>
-            <text x="90.0" y="5.845703125" font-size="12.0" font-family="sans-serif" font-weight="NORMAL" font-style="NORMAL" style="stroke-width:1.0">
+            <text x="${90.0}" y="${5.845703125}" font-size="${12.0}" font-family="sans-serif" font-weight="NORMAL" font-style="NORMAL" style="stroke-width:${1.0}">
             O</text>
-            <text x="10.0" y="85.845703125" font-size="12.0" font-family="sans-serif" font-weight="NORMAL" font-style="NORMAL" style="stroke-width:1.0">
+            <text x="${10.0}" y="${85.845703125}" font-size="${12.0}" font-family="sans-serif" font-weight="NORMAL" font-style="NORMAL" style="stroke-width:${1.0}">
             O</text>
-            <text x="90.0" y="85.845703125" font-size="12.0" font-family="sans-serif" font-weight="NORMAL" font-style="NORMAL" style="stroke-width:1.0">
+            <text x="${90.0}" y="${85.845703125}" font-size="${12.0}" font-family="sans-serif" font-weight="NORMAL" font-style="NORMAL" style="stroke-width:${1.0}">
             O</text>
             </g>
             </svg>
+
         """.trimIndent()
 
         assertEquals(target, svg)
@@ -128,8 +129,8 @@ class TestVizToSVG : TestBase() {
         }.toSVG()
 
         val target = """
-            <?xml version="1.0"?>
-            <svg xmlns="http://www.w3.org/2000/svg" width="100.0" height="100.0">
+            <?xml version="${1.0}"?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="${100.0}" height="${100.0}">
             <g>
             </g>
             </svg>
@@ -155,10 +156,10 @@ class TestVizToSVG : TestBase() {
 
         }.toSVG()
         val expected = """
-            <?xml version="1.0"?>
-            <svg xmlns="http://www.w3.org/2000/svg" width="100.0" height="100.0">
+            <?xml version="${1.0}"?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="${100.0}" height="${100.0}">
             <g>
-            <rect x="10.0" y="10.0" width="100.0" height="100.0" style="stroke-width:1.0"/>
+            <rect x="${10.0}" y="${10.0}" width="${100.0}" height="${100.0}" style="stroke-width:${1.0}"/>
             </g>
             </svg>
 
@@ -182,10 +183,10 @@ class TestVizToSVG : TestBase() {
             }
         }.toSVG()
         val expected = """
-            <?xml version="1.0"?>
-            <svg xmlns="http://www.w3.org/2000/svg" width="100.0" height="100.0">
+            <?xml version="${1.0}"?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="${100.0}" height="${100.0}">
             <g>
-            <line x1="1.0" y1="1.0" x2="70.0" y2="70.0" style="stroke:rgba(173, 255, 47, 1.0);stroke-width:1.0"/>
+            <line x1="${1.0}" y1="${1.0}" x2="${70.0}" y2="${70.0}" style="stroke:rgba(${173}, ${255}, ${47}, ${1.0});stroke-width:${1.0}"/>
             </g>
             </svg>
 
@@ -209,10 +210,10 @@ class TestVizToSVG : TestBase() {
             }
         }.toSVG()
         val expected = """
-            <?xml version="1.0"?>
-            <svg xmlns="http://www.w3.org/2000/svg" width="100.0" height="100.0">
+            <?xml version="${1.0}"?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="${100.0}" height="${100.0}">
             <g>
-            <circle cx="50.0" cy="50.0" r="30.0" style="fill:rgba(0, 128, 0, 1.0);stroke:rgba(255, 0, 0, 1.0);stroke-width:5.0"/>
+            <circle cx="${50.0}" cy="${50.0}" r="${30.0}" style="fill:rgba(${0}, ${128}, ${0}, ${1.0});stroke:rgba(${255}, ${0}, ${0}, ${1.0});stroke-width:${5.0}"/>
             </g>
             </svg>
 
@@ -275,30 +276,30 @@ class TestVizToSVG : TestBase() {
 
         }.toSVG()
 
-        val expect = """<?xml version="1.0"?>
-            |<svg xmlns="http://www.w3.org/2000/svg" width="400.0" height="400.0">
+        val expect = """<?xml version="${1.0}"?>
+            |<svg xmlns="http://www.w3.org/2000/svg" width="${400.0}" height="${400.0}">
             |<g>
-            |<rect x="0.0" y="0.0" width="400.0" height="400.0" style="fill:url('#grad1');stroke-width:1.0"/>
-            |<rect x="30.0" y="30.0" width="340.0" height="340.0" style="fill:rgba(255, 255, 255, 1.0);stroke-width:1.0"/>
-            |<rect x="60.0" y="60.0" width="280.0" height="280.0" style="fill:url('#grad2');stroke-width:1.0"/>
-            |<path style="fill:rgba(255, 255, 255, 1.0);stroke:rgba(0, 0, 0, 0.0);stroke-width:1.0" d="M 200.0 340.0 L 340.0 60.0 L 360.0 360.0 "/>
-            |<path style="fill:rgba(0, 0, 0, 0.0);stroke:rgba(255, 255, 255, 1.0);stroke-width:30.0" d="M 220.0 360.0 L 55.0 37.5 "/>
-            |<path style="fill:rgba(0, 0, 0, 0.0);stroke:rgba(255, 255, 255, 1.0);stroke-width:30.0" d="M 340.0 300.0 L 215.0 37.5 "/>
+            |<rect x="${0.0}" y="${0.0}" width="${400.0}" height="${400.0}" style="fill:url('#grad1');stroke-width:${1.0}"/>
+            |<rect x="${30.0}" y="${30.0}" width="${340.0}" height="${340.0}" style="fill:rgba(${255}, ${255}, ${255}, ${1.0});stroke-width:${1.0}"/>
+            |<rect x="${60.0}" y="${60.0}" width="${280.0}" height="${280.0}" style="fill:url('#grad2');stroke-width:${1.0}"/>
+            |<path style="fill:rgba(${255}, ${255}, ${255}, ${1.0});stroke:rgba(${0}, ${0}, ${0}, ${0.0});stroke-width:${1.0}" d="M 200.0 340.0 L 340.0 60.0 L 360.0 360.0 "/>
+            |<path style="fill:rgba(${0}, ${0}, ${0}, ${0.0});stroke:rgba(${255}, ${255}, ${255}, ${1.0});stroke-width:${30.0}" d="M 220.0 360.0 L 55.0 37.5 "/>
+            |<path style="fill:rgba(${0}, ${0}, ${0}, ${0.0});stroke:rgba(${255}, ${255}, ${255}, ${1.0});stroke-width:${30.0}" d="M 340.0 300.0 L 215.0 37.5 "/>
             |</g>
             |<defs>
-            |<linearGradient id="grad1" x1="0.0 px" y1="400.0 px" x2="400.0 px" y2="0.0 px">
-            |<stop offset="0.0%" style="stop-color:rgba(65, 63, 222, 1.0)"/>
-            |<stop offset="25.0%" style="stop-color:rgba(68, 61, 226, 1.0)"/>
-            |<stop offset="50.0%" style="stop-color:rgba(118, 65, 223, 1.0)"/>
-            |<stop offset="75.0%" style="stop-color:rgba(185, 69, 206, 1.0)"/>
-            |<stop offset="100.0%" style="stop-color:rgba(222, 61, 130, 1.0)"/>
+            |<linearGradient id="grad1" x1="${0.0} px" y1="${400.0} px" x2="${400.0} px" y2="${0.0} px">
+            |<stop offset="${0.0}%" style="stop-color:rgba(${65}, ${63}, ${222}, ${1.0})"/>
+            |<stop offset="${25.0}%" style="stop-color:rgba(${68}, ${61}, ${226}, ${1.0})"/>
+            |<stop offset="${50.0}%" style="stop-color:rgba(${118}, ${65}, ${223}, ${1.0})"/>
+            |<stop offset="${75.0}%" style="stop-color:rgba(${185}, ${69}, ${206}, ${1.0})"/>
+            |<stop offset="${100.0}%" style="stop-color:rgba(${222}, ${61}, ${130}, ${1.0})"/>
             |</linearGradient>
-            |<linearGradient id="grad2" x1="0.0 px" y1="400.0 px" x2="400.0 px" y2="0.0 px">
-            |<stop offset="0.0%" style="stop-color:rgba(65, 63, 222, 1.0)"/>
-            |<stop offset="25.0%" style="stop-color:rgba(68, 61, 226, 1.0)"/>
-            |<stop offset="50.0%" style="stop-color:rgba(118, 65, 223, 1.0)"/>
-            |<stop offset="75.0%" style="stop-color:rgba(185, 69, 206, 1.0)"/>
-            |<stop offset="100.0%" style="stop-color:rgba(222, 61, 130, 1.0)"/>
+            |<linearGradient id="grad2" x1="${0.0} px" y1="${400.0} px" x2="${400.0} px" y2="${0.0} px">
+            |<stop offset="${0.0}%" style="stop-color:rgba(${65}, ${63}, ${222}, ${1.0})"/>
+            |<stop offset="${25.0}%" style="stop-color:rgba(${68}, ${61}, ${226}, ${1.0})"/>
+            |<stop offset="${50.0}%" style="stop-color:rgba(${118}, ${65}, ${223}, ${1.0})"/>
+            |<stop offset="${75.0}%" style="stop-color:rgba(${185}, ${69}, ${206}, ${1.0})"/>
+            |<stop offset="${100.0}%" style="stop-color:rgba(${222}, ${61}, ${130}, ${1.0})"/>
             |</linearGradient>
             |</defs>
             |</svg>
@@ -336,21 +337,21 @@ class TestVizToSVG : TestBase() {
 
         }.toSVG()
 
-        val expect = """<?xml version="1.0"?>
-                |<svg xmlns="http://www.w3.org/2000/svg" width="800.0" height="200.0">
+        val expect = """<?xml version="${1.0}"?>
+                |<svg xmlns="http://www.w3.org/2000/svg" width="${800.0}" height="${200.0}">
                 |<g>
-                |<circle cx="400.0" cy="100.0" r="100.0" style="fill:url('#grad1');stroke-width:1.0"/>
-                |<circle cx="200.0" cy="100.0" r="120.0" style="fill:url('#grad2');stroke-width:1.0"/>
+                |<circle cx="${400.0}" cy="${100.0}" r="${100.0}" style="fill:url('#grad1');stroke-width:${1.0}"/>
+                |<circle cx="${200.0}" cy="${100.0}" r="${120.0}" style="fill:url('#grad2');stroke-width:${1.0}"/>
                 |</g>
                 |<defs>
-                |<radialGradient id="grad1" cx="400.0 px" cy="100.0 px" r="100.0 px">
-                |<stop offset="0.0%" style="stop-color:rgba(255, 105, 180, 1.0)"/>
-                |<stop offset="100.0%" style="stop-color:rgba(135, 206, 235, 1.0)"/>
+                |<radialGradient id="grad1" cx="${400.0} px" cy="${100.0} px" r="${100.0} px">
+                |<stop offset="${0.0}%" style="stop-color:rgba(${255}, ${105}, ${180}, ${1.0})"/>
+                |<stop offset="${100.0}%" style="stop-color:rgba(${135}, ${206}, ${235}, ${1.0})"/>
                 |</radialGradient>
-                |<radialGradient id="grad2" cx="200.0 px" cy="100.0 px" r="120.0 px">
-                |<stop offset="0.0%" style="stop-color:rgba(255, 0, 0, 1.0)"/>
-                |<stop offset="50.0%" style="stop-color:rgba(255, 165, 0, 1.0)"/>
-                |<stop offset="100.0%" style="stop-color:rgba(128, 0, 128, 1.0)"/>
+                |<radialGradient id="grad2" cx="${200.0} px" cy="${100.0} px" r="${120.0} px">
+                |<stop offset="${0.0}%" style="stop-color:rgba(${255}, ${0}, ${0}, ${1.0})"/>
+                |<stop offset="${50.0}%" style="stop-color:rgba(${255}, ${165}, ${0}, ${1.0})"/>
+                |<stop offset="${100.0}%" style="stop-color:rgba(${128}, ${0}, ${128}, ${1.0})"/>
                 |</radialGradient>
                 |</defs>
                 |</svg>
