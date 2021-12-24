5f7644d8b706db42c887167b83e126b27c488d7f
gzoritchak <g.zoritchak@gmail.com>
2021-12-24 11:01:42 +0100

    viz: Fix wrong visibility of IOSVizContainerView.vizList

--- viz/src/iosMain/kotlin/io/data2viz/viz/IOSVizContainerView.kt
+++ viz/src/iosMain/kotlin/io/data2viz/viz/IOSVizContainerView.kt
@@ -79,7 +79,7 @@ internal class IosVizContainer(
 
     override var density: Double = 1.0
 
-    var vizList = mutableListOf<Viz>()
+    override var vizList = mutableListOf<Viz>()
 
     public override fun newViz(init: Viz.() -> Unit): Viz{
         val viz = Viz().apply(init)
